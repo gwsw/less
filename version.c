956c0ddecfd9f61c13b7823222d84444e9f7bfb4
Mark Nudelman <markn@greenwoodsoftware.com>
1996-03-16 08:59:39 +0000

    v306

--- version.c
+++ version.c
@@ -530,6 +530,7 @@ v302   1/31/96	Fix cmd F to notice window-change signals.
 v303   1/31/96	Add ESC-SPACE command.
 v304   2/1/96	Add ^R search modifier; add LESSSECURE.
 v305   2/2/96	Workaround Linux /proc kernel bug; add LESSKEY.
+v306   3/16/96	Minor fixes.
 */
 
-char version[] = "305";
+char version[] = "306";
