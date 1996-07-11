5e653359deb18004d686aaefcb2bdd189a8058a0
Mark Nudelman <markn@greenwoodsoftware.com>
1996-07-11 18:46:56 +0000

    v320

--- version.c
+++ version.c
@@ -546,6 +546,7 @@ v317   5/14/96	Various fixes for the MS-DOS and OS/2 builds.
 v318   5/29/96	Port to OS-9 Microware compiler; minor fixes 
 		(thanks to Martin Gregorie).
 v319   7/8/96	Fix Windows port (thanks to Jeff Paquette).
+v320   7/11/96	Final fixes for Windows port.
 */
 
-char version[] = "319";
+char version[] = "320";
