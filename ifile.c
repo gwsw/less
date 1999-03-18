be6f9b16fb30bf3a3643b72616ec2006b8c1e3cb
Mark Nudelman <markn@greenwoodsoftware.com>
1999-03-18 05:48:47 +0000

    Don't use sparse initializers.

--- ifile.c
+++ ifile.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1984,1985,1989,1994,1995,1996  Mark Nudelman
+ * Copyright (c) 1984,1985,1989,1994,1995,1996,1999  Mark Nudelman
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -61,7 +61,8 @@ struct ifile {
 /*
  * Anchor for linked list.
  */
-static struct ifile anchor = { &anchor, &anchor, 0 };
+static struct ifile anchor = { &anchor, &anchor, NULL, NULL, 0, 0, '\0',
+                               { NULL_POSITION, 0 } };
 static int ifiles = 0;
 
 	static void
