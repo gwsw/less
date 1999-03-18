4625978f4afb0f1bbc48d71f9da8940abdb6d738
Mark Nudelman <markn@greenwoodsoftware.com>
1999-03-18 05:52:58 +0000

    Quit after reading 20 consecutive nulls (fix for #143).

--- os.c
+++ os.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1984,1985,1989,1994,1995,1996  Mark Nudelman
+ * Copyright (c) 1984,1985,1989,1994,1995,1996,1999  Mark Nudelman
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -145,6 +145,26 @@ iread(fd, buf, len)
 	}
 #endif
 	n = read(fd, buf, len);
+#if 1
+	/*
+	 * This is a kludge to workaround a problem on some systems
+	 * where terminating a remote tty connection causes read() to
+	 * start returning 0 forever, instead of -1.
+	 */
+	{
+		extern int ignore_eoi;
+		if (!ignore_eoi)
+		{
+			static int consecutive_nulls = 0;
+			if (n == 0)
+				consecutive_nulls++;
+			else
+				consecutive_nulls = 0;
+			if (consecutive_nulls > 20)
+				quit(QUIT_ERROR);
+		}
+	}
+#endif
 	reading = 0;
 	if (n < 0)
 		return (-1);
