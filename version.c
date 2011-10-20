34470e55f090980b3ed896669b2377ea2b51811a
Mark Nudelman <markn@greenwoodsoftware.com>
2011-10-20 00:21:01 +0000

    v445

--- version.c
+++ version.c
@@ -744,8 +744,10 @@ v442  3/2/11    Fix search bug.
 v443  4/9/11    Fix Windows build.
 v444  6/8/11    Fix ungetc bug; remove vestiges of obsolete -l option.
 -----------------------------------------------------------------
-v445  10/16/11  Fix hilite bug in backwards scroll with -J.
+v445  10/19/11  Fix hilite bug in backwards scroll with -J.
                 Fix hilite bug with backspaces.
+                Improve handling when CR is used to overstrike.
+		Fix bugs handling SGR sequences in Win32 (thanks to Eric Lee).
 */
 
 char version[] = "445";
