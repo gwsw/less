7d603c53dc9b2d20eac9331830c3e4398bc7ee2f
Mark Nudelman <markn@greenwoodsoftware.com>
2014-09-03 01:00:58 +0000

    Fix leak in error path.

--- regexp.c
+++ regexp.c
@@ -243,7 +243,10 @@ char *exp;
 	regcode = r->program;
 	regc(MAGIC);
 	if (reg(0, &flags) == NULL)
+	{
+		free(r);
 		return(NULL);
+	}
 
 	/* Dig out information for optimizations. */
 	r->regstart = '\0';	/* Worst-case defaults. */
