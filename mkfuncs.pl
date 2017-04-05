f136880c71212651dbd6c8209f1b61a5ffb6a0e8
Mark Nudelman <markn@greenwoodsoftware.com>
2017-04-05 20:18:33 +0000

    Initial revision

---
+++ mkfuncs.pl
@@ -0,0 +1,23 @@
+#! /usr/bin/perl
+use strict;
+
+my $state = 0;
+my $def;
+
+while (<>) {
+	if (/^\tpublic\s+(.*)/) {
+		$def = "public $1";
+		$state = 1;
+	} elsif ($state == 1 and /(\w+)\s*\(/) {
+		$def .= " $1 LPARAM((";
+		$state = 2;
+	} elsif ($state == 2) {
+		if (/^{/) {
+			print "$def));\n";
+			$state = 0;
+		} elsif (/^\s*([^;]*)/) {
+			$def .= ', ' if substr($def,-1) ne '(';
+			$def .= $1;
+		}
+	}
+}
