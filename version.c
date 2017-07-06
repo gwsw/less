9aadfbb3b9719f2c4cd2823bfb775e5795ba9d18
Mark Nudelman <markn@greenwoodsoftware.com>
2017-07-06 02:09:18 +0000

    v497

--- version.c
+++ version.c
@@ -810,6 +810,7 @@ v494  6/26/17   Update Unicode tables; make Cf chars composing not binary.
 v495  7/3/17    Improve binary file detection (thanks to Bela Lubkin);
                 do -R filter when matching tags (thanks to Matthew Malcomson).
 v496  7/5/17    Add LESSRSCROLL marker.
+v497  7/5/17    Sync.
 */
 
-char version[] = "496";
+char version[] = "497";
