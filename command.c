a85df588c24c01ef9c9fa800e2f1db339df86563
Brooks Davis <brooks@one-eyed-alien.net>
2019-08-01 12:08:55 -0700

    Use ANSI prototypes for static functions. (#17)
    Some ABIs require prototypes in order to call functions correctly.

--- command.c
+++ command.c
@@ -81,7 +81,7 @@ struct ungot {
 };
 static struct ungot* ungot = NULL;
 
-static void multi_search();
+static void multi_search LESSPARAMS((char *pattern, int n, int silent));
 
 /*
  * Move the cursor to start of prompt line before executing a command.
