1c533a6ec44ccf50a6155f5f77f02f20820c7f40
Mark Nudelman <markn@greenwoodsoftware.com>
2020-03-19 15:21:39 -0700

    Don't use PATH_MAX.

--- filename.c
+++ filename.c
@@ -795,9 +795,9 @@ lrealpath(path)
 	char *path;
 {
 #if HAVE_REALPATH
-	char rpath[PATH_MAX];
-	if (realpath(path, rpath) != NULL)
-		return (save(rpath));
+	char *rpath = realpath(path, NULL);
+    if (rpath != NULL)
+		return (rpath);
 #endif
 	return (save(path));
 }
