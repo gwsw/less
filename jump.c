64eaf976c80cc330401f89c5c2a99846e20361c6
Mark Nudelman <markn@greenwoodsoftware.com>
2020-05-01 16:38:18 -0700

    Revert 58c40b58eca84f5e336cad7f63c2f4e9aea97073. Unintended consequences, such as 'p' in squished state doesn't do anything (should repaint unsquished).

--- jump.c
+++ jump.c
@@ -212,7 +212,7 @@ jump_loc(pos, sline)
 		nline -= sindex;
 		if (nline > 0)
 			forw(nline, position(BOTTOM_PLUS_ONE), 1, 0, 0);
-		else if (nline < 0)
+		else
 			back(-nline, position(TOP), 1, 0);
 #if HILITE_SEARCH
 		if (show_attn)
