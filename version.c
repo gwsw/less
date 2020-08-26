46fe29aa0bc60ed28e374ae913bf75db7cfb9a10
Mark Nudelman <markn@greenwoodsoftware.com>
2020-08-25 17:53:06 -0700

    v564

--- version.c
+++ version.c
@@ -887,7 +887,7 @@ v560  5/3/20    Fix regression when command results in no movement;
 v561  5/11/20   Fix erroneous EOF calculation when F command is interrupted.
 v562  5/19/20   Update Unicode tables; minor doc formatting.
 v563  6/13/20   Fix crash due to realpath() incompatibility.
-v564
+v564  8/25/20   Handle realpath consistently; update docs.
 */
 
-char version[] = "564x";
+char version[] = "564";
