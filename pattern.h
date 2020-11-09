be1923961102d81f6dcd8eb25bf1c0de85f36122
Mark Nudelman <markn@greenwoodsoftware.com>
2020-11-08 21:02:08 -0800

    Change CLEAR_PATTERN to SET_NULL_PATTERN to avoid confusion with clear_pattern.

--- pattern.h
+++ pattern.h
@@ -4,7 +4,7 @@
 #define __USE_GNU 1
 #include <regex.h>
 #define PATTERN_TYPE          struct re_pattern_buffer *
-#define CLEAR_PATTERN(name)   name = NULL
+#define SET_NULL_PATTERN(name)   name = NULL
 #endif
 
 #if HAVE_POSIX_REGCOMP
@@ -15,27 +15,27 @@
 #define REGCOMP_FLAG    0
 #endif
 #define PATTERN_TYPE          regex_t *
-#define CLEAR_PATTERN(name)   name = NULL
+#define SET_NULL_PATTERN(name)   name = NULL
 #endif
 
 #if HAVE_PCRE
 #include <pcre.h>
 #define PATTERN_TYPE          pcre *
-#define CLEAR_PATTERN(name)   name = NULL
+#define SET_NULL_PATTERN(name)   name = NULL
 #endif
 
 #if HAVE_PCRE2
 #define PCRE2_CODE_UNIT_WIDTH 8
 #include <pcre2.h>
 #define PATTERN_TYPE          pcre2_code *
-#define CLEAR_PATTERN(name)   name = NULL
+#define SET_NULL_PATTERN(name)   name = NULL
 #endif
 
 #if HAVE_RE_COMP
 char *re_comp LESSPARAMS ((char*));
 int re_exec LESSPARAMS ((char*));
 #define PATTERN_TYPE          int
-#define CLEAR_PATTERN(name)   name = 0
+#define SET_NULL_PATTERN(name)   name = 0
 #endif
 
 #if HAVE_REGCMP
@@ -43,17 +43,17 @@ char *regcmp LESSPARAMS ((char*));
 char *regex LESSPARAMS ((char**, char*));
 extern char *__loc1;
 #define PATTERN_TYPE          char **
-#define CLEAR_PATTERN(name)   name = NULL
+#define SET_NULL_PATTERN(name)   name = NULL
 #endif
 
 #if HAVE_V8_REGCOMP
 #include "regexp.h"
 extern int reg_show_error;
 #define PATTERN_TYPE          struct regexp *
-#define CLEAR_PATTERN(name)   name = NULL
+#define SET_NULL_PATTERN(name)   name = NULL
 #endif
 
 #if NO_REGEX
 #define PATTERN_TYPE          void *
-#define CLEAR_PATTERN(name)   
+#define SET_NULL_PATTERN(name)   
 #endif
