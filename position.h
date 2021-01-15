f3ebd01bf4bdeb96981dd535b0d8bcad6ce00b7a
Mark Nudelman <markn@greenwoodsoftware.com>
2021-01-15 11:35:59 -0800

    Whitespace cleanup in .h files. Remove tabs.

--- position.h
+++ position.h
@@ -4,8 +4,8 @@
 /*
  * Include file for interfacing to position.c modules.
  */
-#define	TOP		(0)
-#define	TOP_PLUS_ONE	(1)
-#define	BOTTOM		(-1)
-#define	BOTTOM_PLUS_ONE	(-2)
-#define	MIDDLE		(-3)
+#define TOP             (0)
+#define TOP_PLUS_ONE    (1)
+#define BOTTOM          (-1)
+#define BOTTOM_PLUS_ONE (-2)
+#define MIDDLE          (-3)
