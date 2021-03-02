939fbc3e9363a2a288a8702a0c3368f0f8164c50
Mark Nudelman <markn@greenwoodsoftware.com>
2021-03-02 12:20:06 -0800

    v580

--- version.c
+++ version.c
@@ -910,7 +910,7 @@ v576  2/4/21    Make sure search result is visible; add --use-color and --color.
 v577  2/9/21    Use ttyname to get name of tty device.
 v578  2/9/21    Doc
 v579  2/14/21   Fix double-width char bugs and non-match search crash.
-v580
+v580  3/2/21    Some color fixes; fix compiler warnings; some lesstest support.
 */
 
-char version[] = "580x";
+char version[] = "580";
