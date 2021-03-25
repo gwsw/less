6cde79d586de0aa5e6defbea7716430763fad9c3
Charlie Lin <clin@rollins.edu>
2021-03-25 16:07:20 -0400

    Commit more Python versions of scripts. WIP

--- mkutable.py
+++ mkutable.py
@@ -1,12 +1,10 @@
 #! /usr/bin/env python
 
 from argparse import ArgumentParser
-from sys import argv
+from sys import exit
 from subprocess import run
+from fileinput import input
 import re
-import friendly
-
-friendly.install()
 
 # Override Unicode tables for certain control chars
 # that are expected to be found in normal text files.
@@ -42,7 +40,6 @@ def main() -> int:
     opt_n = args.n
     global type_field
     type_field = args.f
-
     types = {type: 1 for type in args.types}
     out = {'types': types, 'prev_code': 0, 'in_run': False, 'run_type': "",
            'start_code': 0}
@@ -75,44 +72,34 @@ def main() -> int:
             type = re.match(r"s/\s//g", type)
             for last_code in range(lo_code, hi_code):
                 output(out, last_code, 'Zs' if force_space[last_code] else 'Mn'
-                       if force_compose[last_code] else type)
+                    if force_compose[last_code] else type)
     output(out, last_code)
     return 1
 
+def output(out: dict, code: int, type: str):
+    type_ok = (type and ${${$out}{types}}{$type})
+    type_ok = not type_ok if opt_n
+    prev_code = out{prev_code}
 
-def output(out: dict, code: int, type: str = None):
-    type_ok = type is not None and type in out['types'].keys()
-    if opt_n:
-        type_ok = not type_ok
-    prev_code = out['prev_code']
-
-    print(out['types'].keys())
-    import pprint
-    p = pprint.PrettyPrinter()
-    p.pprint(out)
-
-    print(type)
     if not type_ok:
         end_run(out, prev_code)
-    elif (not out["in_run"] or type != out["run_type"] or code != prev_code+1):
+    elif (not out{in_run} or type ne out{run_type} or code != prev_code+1):
         end_run(out, prev_code)
         start_run(out, code, type)
-    out[prev_code] = code
-
+    out{prev_code} = code
 
 def start_run(out: dict, code: int, type: str):
-    out["start_code"] = code
-    out["prev_code"] = code
-    out["run_type"] = type
-    out["in_run"] = True
-
+    out[start_code] = code
+    out[prev_code] = code
+    out[run_type] = type
+    out[in_run] = 1
 
 def end_run(out: dict, code: int):
-    if not out["in_run"]:
+    if not out[in_run]:
         return
-    print("\t{ 0x%04x, 0x%04x }, /* %s */" % out["start_code"], code,
-          out["run_type"])
-    out["in_run"] = False
+    print("\t{ 0x%04x, 0x%04x }, /* %s */" % $$out{start_code}, code,
+          out{run_type})
+    out[in_run] = 0
 
 if __name__ == "__main__":
     exit(0 if main() else 1)
