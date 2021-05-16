gwsw/less

commit 143ba3450a7410adcf116fd6aeb0054668dc69f0

Fix handling of ANSI sequences in a wrapped line.

line.c
@@ -269,6 +269,26 @@ plinestart(pos)
 }
 
 /*
+ * Add char to the shifted_ansi buffer.
+ */
+	static void
+add_ansi(ch)
+	char ch;
+{
+	if (shifted_ansi.end == shifted_ansi.size)
+	{
+		/* Expand shifted_ansi buffer. */
+		int size = (shifted_ansi.size == 0) ? 8 : shifted_ansi.size * 2;
+		char *buf = (char *) ecalloc(size, sizeof(char));
+		memcpy(buf, shifted_ansi.buf, shifted_ansi.size);
+		if (shifted_ansi.buf != NULL) free(shifted_ansi.buf);
+		shifted_ansi.buf = buf;
+		shifted_ansi.size = size;
+	}
+	shifted_ansi.buf[shifted_ansi.end++] = ch;
+}
+
+/*
  * Shift line left so that the last char is just to the left
  * of the first visible column.
  */
@@ -276,9 +296,9 @@ plinestart(pos)
 pshift_all(VOID_PARAM)
 {
 	int i;
-	int shift = linebuf.end - linebuf.print;
-	for (i = 0;  i < linebuf.print;  i++)
-		linebuf.buf[i] = linebuf.buf[i + shift];
+	for (i = linebuf.print;  i < linebuf.end;  i++)
+		if (linebuf.attr[i] == AT_ANSI)
+			add_ansi(linebuf.buf[i]);
 	linebuf.end = linebuf.print;
 	end_column = linebuf.pfx_end;
 }
@@ -635,23 +655,10 @@ store_char(ch, a, rep, pos)
 	if (cshift < hshift)
 	{
 		if (a == AT_ANSI)
-		{
-			if (shifted_ansi.end == shifted_ansi.size)
-			{
-				/* Expand shifted_ansi buffer. */
-				int size = (shifted_ansi.size == 0) ? 8 : shifted_ansi.size * 2;
-				char *buf = (char *) ecalloc(size, sizeof(char));
-				memcpy(buf, shifted_ansi.buf, shifted_ansi.size);
-				if (shifted_ansi.buf != NULL) free(shifted_ansi.buf);
-				shifted_ansi.buf = buf;
-				shifted_ansi.size = size;
-			}
-			shifted_ansi.buf[shifted_ansi.end++] = ch;
-		}
+			add_ansi(ch);
 		if (linebuf.end > linebuf.print)
 		{
-			for (i = 0;  i < linebuf.print;  i++)
-				linebuf.buf[i] = linebuf.buf[i + replen];
+			memcpy(&linebuf.buf[0], &linebuf.buf[replen], linebuf.print);
 			linebuf.end -= replen;
 			cshift += w;
 			while (cshift > hshift)

commit 03fdb741791a667dabbd5ef1aeba82b930671677

Fix crash due to memcpy with overlapping objects
According to the POSIX specification of memcpy(3). If a copy takes place
between objects that overlap, the behaviour is undefined. Unfortunately,
horizontal scrolling contained two such memcpy invocations which lead to
crash on Alpine Linux. Since The _FORTIFY_SOURCE implementation used on
Alpine Linux, checks for memcpy invocations with overlapping objects and
crashes the program when such an invocation is found. On Alpine this
crash can be reproduced by opening a file which exceeds the terminal
width in the horizontal dimension and then attempting to scroll
horizontally.

This commit fixes this issue by using memmove(3) instead of memcpy(3),
thereby fixing a regression introduced in 143ba345 and d7f6d4ec.

line.c
@@ -740,8 +740,8 @@ store_char(ch, a, rep, pos)
 		if (linebuf.end > linebuf.print)
 		{
 			/* Shift left enough to put last byte of this char at print-1. */
-			memcpy(&linebuf.buf[0], &linebuf.buf[replen], linebuf.print);
-			memcpy(&linebuf.attr[0], &linebuf.attr[replen], linebuf.print);
+			memmove(&linebuf.buf[0], &linebuf.buf[replen], linebuf.print);
+			memmove(&linebuf.attr[0], &linebuf.attr[replen], linebuf.print);
 			linebuf.end -= replen;
 			cshift += w;
 			/*