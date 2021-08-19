d38a3c6ce9ef3139fca10dc305c56aa191407b79
Charlie Lin <clin@rollins.edu>
2021-08-19 12:23:36 -0400

    Touch up a few Python scripts

--- mkfuncs.py
+++ mkfuncs.py
@@ -1,28 +1,28 @@
 #!/usr/bin/env python
 
 import fileinput
-import re
+from re import search
 
 definition = ''
 state = 0
 params = 0
 
 for line in fileinput.input():
-    if test := re.search(r'^\tpublic\s+(.*)', line):
+    if test := search(r'^\tpublic\s+(.*)', line):
         definition = 'public ' + test.group(1)
         state = 1
         params = 0
-    elif (state == 1) and (test := re.search(r'(\w+)\s*\(', line)):
-        definition = '{0} LESSPARAMS (('.format(test.group(1))
+    elif (state == 1) and (test := search(r'(\w+)\s*\(', line)):
+        definition = f'{test.group(1)} LESSPARAMS (('
         state = 2
     elif state == 2:
-        if re.search(r'^{', line):
+        if search(r'^{', line):
             if not params:
                 definition += 'VOID_PARAM'
             print(f'{definition}));')
             state = 0
-        elif test := re.search(r'^\s*([^;]*)', line):
-            if (definition[-1:] != '('):
+        elif test := search(r'^\s*([^;]*)', line):
+            if (definition[-1] != '('):
                 definition += ', '
             definition += test.group(1)
             params = 1
