gwsw/less

commit d081e3e79b2e731fa9144d51a85abc26dad79d50

Add --redraw-on-quit option.

main.c
@@ -60,8 +60,8 @@ extern int      know_dumb;
 extern int      pr_type;
 extern int      quit_if_one_screen;
 extern int      no_init;
-extern int errmsgs;
-
+extern int      errmsgs;
+extern int      redraw_on_quit;
 
 /*
  * Entry point.
@@ -409,12 +409,17 @@ quit(status)
 	rstat('Q');
 #endif /*LESSTEST*/
 	quitting = 1;
-	edit((char*)NULL);
 	save_cmdhist();
 	if (interactive())
 		clear_bot();
 	deinit();
 	flush();
+	if (redraw_on_quit)
+	{
+		repaint();
+		flush();
+	}
+	edit((char*)NULL);
 	raw_mode(0);
 #if MSDOS_COMPILER && MSDOS_COMPILER != DJGPPC
 	/* 

less.hlp
@@ -223,6 +223,8 @@
                   Remove duplicates from command history.
                 --no-number-headers
                   Don't give line numbers to header lines.
+                --redraw-on-quit
+                  Redraw final screen when quitting.
                 --rscroll=C
                   Set the character used to mark truncated lines.
                 --save-marks

less.nro.VER
@@ -1159,6 +1159,14 @@ This option changes the character used to mark truncated lines.
 It may begin with a two-character attribute indicator like LESSBINFMT does.
 If there is no attribute indicator, standout is used.
 If set to "\-", truncated lines are not marked.
+.IP "\-\-redraw-on-quit"
+When quitting, after sending the terminal deinitialization string,
+redraws the entire last screen.
+On terminals whose terminal deinitialization string causes the
+terminal to switch from an alternate screen, 
+this makes the last screenful of the current file remain visible after
+.I less
+has quit.
 .IP "\-\-save-marks"
 Save marks in the history file, so marks are retained
 across different invocations of

opttbl.c
@@ -68,6 +68,7 @@ public int status_line;         /* Highlight entire marked lines */
 public int header_lines;        /* Freeze header lines at top of screen */
 public int header_cols;         /* Freeze header columns at left of screen */
 public int nonum_headers;       /* Don't give headers line numbers */
+public int redraw_on_quit;      /* Redraw last screen after term deinit */
 #if HILITE_SEARCH
 public int hilite_search;       /* Highlight matched search patterns? */
 #endif
@@ -146,6 +147,7 @@ static struct optname want_filesize_optname = { "file-size",     NULL };
 static struct optname status_line_optname = { "status-line",     NULL };
 static struct optname header_optname = { "header",               NULL };
 static struct optname nonum_headers_optname = { "no-number-headers", NULL };
+static struct optname redraw_on_quit_optname = { "redraw-on-quit", NULL };
 #if LESSTEST
 static struct optname ttyin_name_optname = { "tty",              NULL };
 static struct optname rstat_optname  = { "rstat",                NULL };
@@ -590,6 +592,14 @@ static struct loption option[] =
 		{
 			"Number header lines",
 			"Don't number header lines",
+			NULL
+		}
+	},
+	{ OLETTER_NONE, &redraw_on_quit_optname,
+		BOOL, OPT_OFF, &redraw_on_quit, NULL,
+		{
+			"Don't redraw screen when quitting",
+			"Redraw last screen when quitting",
 			NULL
 		}
 	},

search.c
@@ -171,7 +171,7 @@ set_pattern(info, pattern, search_type, show_error)
 	 * -i is set AND the pattern is all lowercase.
 	 */
 	is_ucase_pattern = (pattern == NULL) ? FALSE : is_ucase(pattern);
-    is_caseless = (is_ucase_pattern && caseless != OPT_ONPLUS) ? 0 : caseless;
+	is_caseless = (is_ucase_pattern && caseless != OPT_ONPLUS) ? 0 : caseless;
 #if !NO_REGEX
 	if (pattern == NULL)
 		SET_NULL_PATTERN(info->compiled);

version.c
@@ -935,7 +935,7 @@ v591  8/8/21    Use \kB for backspace key in lesskey; add more \k codes;
                 handle multibyte chars in prompt.
 v592  8/24/21   Add --status-line option; limit use of /proc kludge; add --header.
 v593  8/30/21   Add header columns, --no-number-headers.
-v594            Let regex library handle caseless when possible.
+v594            Let regex library handle caseless; add --redraw-on-quit option.
 */
 
 char version[] = "594x";

commit 03c4c93994fb5656981fe4873af7fb8e51583366

Fix regression where lastmark isn't saved
We should be calling save_cmdhist() after edit() otherwise lastmarks
don't get saved.

Signed-off-by: William Casarin <jb55@jb55.com>
Fixes: d081e3e79b2e731fa9144d51a85abc26dad79d50 ('Add --redraw-on-quit option.')

main.c
@@ -411,7 +411,6 @@ quit(status)
 	rstat('Q');
 #endif /*LESSTEST*/
 	quitting = 1;
-	save_cmdhist();
 	if (interactive())
 		clear_bot();
 	deinit();
@@ -428,6 +427,7 @@ quit(status)
 		flush();
 	}
 	edit((char*)NULL);
+	save_cmdhist();
 	raw_mode(0);
 #if MSDOS_COMPILER && MSDOS_COMPILER != DJGPPC
 	/* 