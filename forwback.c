gwsw/less

commit 30c1fb48a962b8dee8107eafa4108070e7cbe4fa

This seems risky, but I can't think of a scenario where there is a problem. In all cases the screen appearance is either the same or better after this change. ...{d}

forwback.c
@@ -399,7 +399,7 @@ forw(n, pos, force, only_last, nblank)
 	else
 	{
 		overlay_header();
-		lower_left();
+		/* lower_left(); {{ considered harmful? }} */
 	}
 	first_time = 0;
 	(void) currline(BOTTOM);

commit 6b785cd283e40bc616c3a770bbad502ed6b38190

Fix --header display, broken in 30c1fb48a962b8dee8107eafa4108070e7cbe4fa.

forwback.c
@@ -206,6 +206,8 @@ overlay_header(VOID_PARAM)
 		}
 		moved = TRUE;
 	}
+	if (moved)
+		lower_left();
 	return moved;
 }
 