1b01b0e4c7b7e0aa8087835452a277778450e0a7
Mark Nudelman <markn@greenwoodsoftware.com>
2022-10-09 14:17:36 -0700

    lesstest: log LESS_TERMCAP_?? vars so termcap keys etc are correct when running test.

--- lesstest/env.c
+++ lesstest/env.c
@@ -63,30 +63,36 @@ static int is_less_env(const char* name, int name_len) {
 }
 
 static void env_setup(EnvBuf* env, char* const* prog_env, int interactive) {
-	env_addpair(env, "LESS_TERMCAP_am", "1");
-	env_addpair(env, "LESS_TERMCAP_cd", "\33S");
-	env_addpair(env, "LESS_TERMCAP_ce", "\33L");
-	env_addpair(env, "LESS_TERMCAP_cl", "\33A");
-	env_addpair(env, "LESS_TERMCAP_cr", "\33<");
-	env_addpair(env, "LESS_TERMCAP_cm", "\33%p2%d;%p1%dj");
-	env_addpair(env, "LESS_TERMCAP_ho", "\33h");
-	env_addpair(env, "LESS_TERMCAP_ll", "\33l");
-	env_addpair(env, "LESS_TERMCAP_mb", "\33b");
-	env_addpair(env, "LESS_TERMCAP_md", "\33d");
-	env_addpair(env, "LESS_TERMCAP_md", "\33e");
-	env_addpair(env, "LESS_TERMCAP_se", "\33t");
-	env_addpair(env, "LESS_TERMCAP_so", "\33s");
-	env_addpair(env, "LESS_TERMCAP_sr", "\33r");
-	env_addpair(env, "LESS_TERMCAP_ue", "\33v");
-	env_addpair(env, "LESS_TERMCAP_uo", "\33u");
-	env_addpair(env, "LESS_TERMCAP_vb", "\33g");
-	env_addpair(env, "LESS_TERMCAP_kr", terminfo.key_right ? terminfo.key_right : "");
-	env_addpair(env, "LESS_TERMCAP_kl", terminfo.key_left ? terminfo.key_left : "");
-	env_addpair(env, "LESS_TERMCAP_ku", terminfo.key_up ? terminfo.key_up : "");
-	env_addpair(env, "LESS_TERMCAP_kd", terminfo.key_down ? terminfo.key_down : "");
-	env_addpair(env, "LESS_TERMCAP_kh", terminfo.key_home ? terminfo.key_home : "");
-	env_addpair(env, "LESS_TERMCAP_@7", terminfo.key_end ? terminfo.key_end : "");
-
+	struct tcvar { char const* name; char const* value; } tcvars[] = {
+		{ "LESS_TERMCAP_am", "1" },
+		{ "LESS_TERMCAP_cd", "\33S" },
+		{ "LESS_TERMCAP_ce", "\33L" },
+		{ "LESS_TERMCAP_cl", "\33A" },
+		{ "LESS_TERMCAP_cr", "\33<" },
+		{ "LESS_TERMCAP_cm", "\33%p2%d;%p1%dj" },
+		{ "LESS_TERMCAP_ho", "\33h" },
+		{ "LESS_TERMCAP_ll", "\33l" },
+		{ "LESS_TERMCAP_mb", "\33b" },
+		{ "LESS_TERMCAP_md", "\33d" },
+		{ "LESS_TERMCAP_md", "\33e" },
+		{ "LESS_TERMCAP_se", "\33t" },
+		{ "LESS_TERMCAP_so", "\33s" },
+		{ "LESS_TERMCAP_sr", "\33r" },
+		{ "LESS_TERMCAP_ue", "\33v" },
+		{ "LESS_TERMCAP_uo", "\33u" },
+		{ "LESS_TERMCAP_vb", "\33g" },
+		{ "LESS_TERMCAP_kr", terminfo.key_right ? terminfo.key_right : "" },
+		{ "LESS_TERMCAP_kl", terminfo.key_left ? terminfo.key_left : "" },
+		{ "LESS_TERMCAP_ku", terminfo.key_up ? terminfo.key_up : "" },
+		{ "LESS_TERMCAP_kd", terminfo.key_down ? terminfo.key_down : "" },
+		{ "LESS_TERMCAP_kh", terminfo.key_home ? terminfo.key_home : "" },
+		{ "LESS_TERMCAP_@7", terminfo.key_end ? terminfo.key_end : "" },
+	};
+	for (int i = 0; i < countof(tcvars); ++i) {
+		struct tcvar* tc = &tcvars[i];
+		env_addpair(env, tc->name, tc->value);
+		log_env(tc->name, strlen(tc->name), tc->value);
+	}
 	for (char* const* envp = prog_env; *envp != NULL; ++envp) {
 		const char* ename = *envp;
 		const char* eq = strchr(ename, '=');
