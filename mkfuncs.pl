0e08859904638d6bb82b4691125ae16361e9d997
David Schlachter <github@schlachter.ca>
2022-12-27 22:52:05 -0500

    Update header generation scripts

--- mkfuncs.pl
+++ mkfuncs.pl
@@ -10,18 +10,11 @@ while (<>) {
 		$def = "public $1";
 		$state = 1;
 		$params = 0;
-	} elsif ($state == 1 and /(\w+)\s*\(/) {
-		$def .= " $1 LESSPARAMS ((";
+	} elsif ($state == 1 and /(\w+)\s*\((.*)\)/) {
+		$def .= " $1 LESSPARAMS (($2))";
 		$state = 2;
 	} elsif ($state == 2) {
-		if (/^{/) {
-			$def .= 'VOID_PARAM' if not $params;
-			print "$def));\n";
-			$state = 0;
-		} elsif (/^\s*([^;]*)/) {
-			$def .= ', ' if substr($def,-1) ne '(';
-			$def .= $1;
-			$params = 1;
-		}
+		print "$def;\n";
+		$state = 0;
 	}
 }
