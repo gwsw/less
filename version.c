f1460ceac6319fd1c4db218c3856cb8427e59fa7
Mark Nudelman <markn@greenwoodsoftware.com>
2023-04-06 10:26:08 -0700

    v632

--- version.c
+++ version.c
@@ -980,7 +980,7 @@ v628  2/20/23   Don't require newline after +&...
 v629  2/26/23   Delay "waiting for data" message for 500 ms.
 v630  3/18/23   Add LESS_DATA_DELAY.
 v631  3/26/23   Fix input of dead keys on Windows.
-v632
+v632  4/6/23    Make lesstest work on MacOS; minor fixes.
 */
 
-char version[] = "632x";
+char version[] = "632";
