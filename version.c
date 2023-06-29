60050bd4053ea6c1f916561e0548b4001e707996
Mark Nudelman <markn@greenwoodsoftware.com>
2023-06-28 20:58:16 -0700

    v637

--- version.c
+++ version.c
@@ -986,7 +986,7 @@ v634  5/29/23   Allow program piping into less to access tty;
                 fix search modifier ^E after ^W.
 v635  6/2/23    Fix crash with ! search modifier.
 v636  6/18/23   Fix -D in MS-DOS build; fix mouse wheel in MS-DOS build.
-v637
+v637  6/28/23   Fix early EOF when SIGWINCH is received.
 */
 
-char version[] = "637x";
+char version[] = "637";
