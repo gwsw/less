0cd542724f31676cfbc3e05c2be796f81af3adc4
Mark Nudelman <markn@greenwoodsoftware.com>
2023-08-29 12:55:06 -0700

    Doc.

--- version.c
+++ version.c
@@ -994,7 +994,7 @@ v641  7/10/23   Fix release.
 v642  7/10/23   Fix release.
 v643  7/20/23   Fix crash on Windows with -o.
 v644            Improve ^C on non-terminated pipe; fix crash when files are
-                deleted; support large files on Windows; fix # bug.
+                deleted; support large files and non-BMP chars on Windows; fix # bug.
 */
 
 char version[] = "644x";
