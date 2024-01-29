gwsw/less

commit 09377f769d2f28f0892f152fd57260b9bd240eb6

Allow expansion of environment variables in lesskey files, using the syntax ${NAME} and ${NAME/STRING/REPL}. Change lesskey parsing so that definitions override earlier definitions.

decode.c
@@ -37,6 +37,8 @@ extern int erase_char, erase2_char, kill_char;
 extern int mousecap;
 extern int sc_height;
 
+static void add_content_table(int (*call_lesskey)(char *, int), char *envname, int sysvar);
+
 #define SK(k) \
 	SK_SPECIAL_KEY, (k), 6, 1, 1, 1
 /*
@@ -319,7 +321,6 @@ public void expand_cmd_tables(void)
 	expand_cmd_table(list_sysvar_tables);
 }
 
-
 /*
  * Initialize the command lists.
  */
@@ -367,6 +368,9 @@ public void init_cmds(void)
 		add_hometable(lesskey, "LESSKEY", LESSKEYFILE, 0);
 	}
 #endif
+	
+	add_content_table(lesskey_content, "LESSKEY_CONTENT_SYSTEM", 1);
+	add_content_table(lesskey_content, "LESSKEY_CONTENT", 0);
 }
 
 /*
@@ -389,9 +393,40 @@ static int add_cmd_table(struct tablelist **tlist, char *buf, int len)
 	}
 	t->t_start = buf;
 	t->t_end = buf + len;
-	t->t_next = *tlist;
-	*tlist = t;
+	t->t_next = NULL;
+	if (*tlist == NULL)
+		*tlist = t;
+	else
+	{
+		struct tablelist *e;
+		for (e = *tlist;  e->t_next != NULL;  e = e->t_next)
+			continue;
+		e->t_next = t;
+	}
 	return (0);
+}
+
+/*
+ * Remove the last command table in a list.
+ */
+static void pop_cmd_table(struct tablelist **tlist)
+{
+	struct tablelist *t;
+	if (*tlist == NULL)
+		return;
+	if ((*tlist)->t_next == NULL)
+	{
+		t = *tlist;
+		*tlist = NULL;
+	} else
+	{
+		struct tablelist *e;
+		for (e = *tlist;  e->t_next->t_next != NULL;  e = e->t_next)
+			continue;
+		t = e->t_next;
+		e->t_next = NULL;
+	}
+	free(t);
 }
 
 /*
@@ -417,7 +452,11 @@ public void add_ecmd_table(char *buf, int len)
  */
 static void add_var_table(struct tablelist **tlist, char *buf, int len)
 {
-	if (add_cmd_table(tlist, buf, len) < 0)
+	struct xbuffer xbuf;
+
+	xbuf_init(&xbuf);
+	expand_evars(buf, len, &xbuf);
+	if (add_cmd_table(tlist, xbuf_char_data(&xbuf), xbuf.end) < 0)
 		error("Warning: environment variables from lesskey file unavailable", NULL_PARG);
 }
 
@@ -566,7 +605,7 @@ static int cmd_search(char *cmd, char *table, char *endtable, char **sp)
 {
 	char *p;
 	char *q;
-	int a;
+	int a = A_INVALID;
 
 	*sp = NULL;
 	for (p = table, q = cmd;  p < endtable;  p++, q++)
@@ -601,13 +640,15 @@ static int cmd_search(char *cmd, char *table, char *endtable, char **sp)
 				if (a & A_EXTRA)
 				{
 					*sp = ++p;
+					while (*p != '\0')
+						++p;
 					a &= ~A_EXTRA;
 				}
 				if (a == A_X11MOUSE_IN)
 					a = x11mouse_action(0);
 				else if (a == A_X116MOUSE_IN)
 					a = x116mouse_action(0);
-				return (a);
+				q = cmd-1;
 			}
 		} else if (*q == '\0')
 		{
@@ -643,10 +684,7 @@ static int cmd_search(char *cmd, char *table, char *endtable, char **sp)
 			q = cmd-1;
 		}
 	}
-	/*
-	 * No match found in the entire command table.
-	 */
-	return (A_INVALID);
+	return (a);
 }
 
 /*
@@ -659,17 +697,22 @@ static int cmd_decode(struct tablelist *tlist, char *cmd, char **sp)
 	int action = A_INVALID;
 
 	/*
-	 * Search thru all the command tables.
-	 * Stop when we find an action which is not A_INVALID.
+	 * Search for the cmd thru all the command tables.
+	 * If we find it more than once, take the last one.
 	 */
+	*sp = NULL;
 	for (t = tlist;  t != NULL;  t = t->t_next)
 	{
-		action = cmd_search(cmd, t->t_start, t->t_end, sp);
-		if (action != A_INVALID)
-			break;
+		char *tsp;
+		int taction = cmd_search(cmd, t->t_start, t->t_end, &tsp);
+		if (taction == A_UINVALID)
+			taction = A_INVALID;
+		if (taction != A_INVALID)
+		{
+			action = taction;
+			*sp = tsp;
+		}
 	}
-	if (action == A_UINVALID)
-		action = A_INVALID;
 	return (action);
 }
 
@@ -708,6 +751,38 @@ public char * lgetenv(char *var)
 	if (a == EV_OK)
 		return (s);
 	return (NULL);
+}
+
+/*
+ * Like lgetenv, but also uses a buffer partially filled with an env table.
+ */
+public char * lgetenv_ext(char *var, char *env_buf, int env_buf_len)
+{
+	char *r;
+	int e;
+	int env_end = 0;
+
+	for (e = 0;;)
+	{
+		for (; e < env_buf_len; e++)
+			if (env_buf[e] == '\0')
+				break;
+		if (e >= env_buf_len) break;
+		if (env_buf[++e] & A_EXTRA)
+		{
+			for (e = e+1; e < env_buf_len; e++)
+				if (env_buf[e] == '\0')
+					break;
+		}
+		e++;
+		if (e >= env_buf_len) break;
+		env_end = e;
+	}
+	/* Temporarily add env_buf to var_tables, do the lookup, then remove it. */
+	add_var_table(&list_var_tables, env_buf, env_end);
+	r = lgetenv(var);
+	pop_cmd_table(&list_var_tables);
+	return r;
 }
 
 /*
@@ -946,7 +1021,18 @@ public int add_hometable(int (*call_lesskey)(char *, int), char *envname, char *
 	free(filename);
 	return (r);
 }
-#endif
+
+/*
+ * Add the content of a lesskey source file.
+ */
+static void add_content_table(int (*call_lesskey)(char *, int), char *envname, int sysvar)
+{
+	char *content = lgetenv(envname);
+	if (isnullenv(content))
+		return;
+	lesskey_content(content, sysvar);
+}
+#endif /* USERFILE */
 
 /*
  * See if a char is a special line-editing command.

Makefile.aut
@@ -17,7 +17,7 @@ endif
 
 SRC = \
 	main.c screen.c brac.c ch.c charset.c cmdbuf.c \
-	command.c cvt.c decode.c edit.c filename.c forwback.c \
+	command.c cvt.c decode.c edit.c evar.c filename.c forwback.c \
 	help.c ifile.c input.c jump.c line.c linenum.c \
 	lsystem.c mark.c optfunc.c option.c opttbl.c os.c \
 	output.c pattern.c position.c prompt.c search.c signal.c \

Makefile.in
@@ -48,7 +48,7 @@ SHELL = /bin/sh
 
 OBJ = \
 	main.${O} screen.${O} brac.${O} ch.${O} charset.${O} cmdbuf.${O} \
-	command.${O} cvt.${O} decode.${O} edit.${O} filename.${O} forwback.${O} \
+	command.${O} cvt.${O} decode.${O} edit.${O} evar.${O} filename.${O} forwback.${O} \
 	help.${O} ifile.${O} input.${O} jump.${O} \
 	line.${O} linenum.${O} \
 	lsystem.${O} mark.${O} optfunc.${O} option.${O} opttbl.${O} os.${O} \

evar.c
@@ -1,0 +1,191 @@
+/*
+ * Copyright (C) 1984-2023  Mark Nudelman
+ *
+ * You may distribute under the terms of either the GNU General Public
+ * License or the Less License, as specified in the README file.
+ *
+ * For more information, see the README file.
+ */
+
+/*
+ * Code to support expanding environment variables in text.
+ */
+
+#include "less.h"
+#include "xbuf.h"
+
+struct replace {
+	struct replace *r_next;
+	char *r_fm;
+	char *r_to;
+};
+
+/*
+ * Skip to the next unescaped slash or right curly bracket in a string.
+ */
+static int skipsl(constant char *buf, int len, int e)
+{
+	int esc = 0;
+	while (e < len && buf[e] != '\0' && (esc || (buf[e] != '/' && buf[e] != '}')))
+	{
+		esc = (!esc && buf[e] == '\\');
+		++e;
+	}
+	return e;
+}
+
+/*
+ * Parse a replacement string: one or more instances of
+ * (slash, pattern, slash, replacement), followed by right curly bracket.
+ * Replacement may be empty in which case the second slash is optional.
+ */
+static struct replace * make_replaces(char *buf, int len, int *pe, char term)
+{
+	int e = *pe;
+	struct replace *replaces = NULL;
+
+	while (term == '/')
+	{
+		struct replace *repl;
+		int to;
+		int fm = e;
+		e = skipsl(buf, len, e);
+		if (e >= len) break;
+		if (e == fm) /* missing fm string; we're done */
+		{
+			while (e < len)
+				if (buf[e++] == '}') break;
+			break;
+		}
+		term = buf[e];
+		buf[e++] = '\0'; /* terminate the fm string */
+		if (term != '/') /* missing to string */
+		{
+			to = e-1;
+		} else
+		{
+			to = e;
+			e = skipsl(buf, len, e);
+			if (e >= len) break;
+			term = buf[e];
+			buf[e++] = '\0'; /* terminate the to string */
+		}
+		repl = malloc(sizeof(struct replace));
+		repl->r_fm = &buf[fm];
+		repl->r_to = &buf[to];
+		repl->r_next = replaces;
+		replaces = repl;
+	}
+	*pe = e;
+	return replaces;
+}
+
+/*
+ * Free a list of replace structs.
+ */
+static void free_replaces(struct replace *replaces)
+{
+	while (replaces != NULL)
+	{
+		struct replace *r = replaces;
+		replaces = r->r_next;
+		free(r);
+	}
+}
+
+/*
+ * See if the initial substring of a string matches a pattern.
+ * Backslash escapes in the pattern are ignored.
+ * Return the length of the matched substring, or 0 if no match.
+ */
+static int evar_match(constant char *str, constant char *pat)
+{
+	int len = 0;
+	while (*pat != '\0')
+	{
+		if (*pat == '\\') ++pat;
+		if (*str++ != *pat++) return 0;
+		++len;
+	}
+	return len;
+}
+
+/*
+ * Find the replacement for a string (&evar[*pv]),
+ * given a list of replace structs.
+ */
+static char * find_replace(constant struct replace *repl, constant char *evar, int *pv)
+{
+	for (;  repl != NULL;  repl = repl->r_next)
+	{
+		int len = evar_match(&evar[*pv], repl->r_fm);
+		if (len > 0)
+		{
+			*pv += len;
+			return repl->r_to;
+		}
+	}
+	return NULL;
+}
+
+/*
+ * With buf[e] positioned just after NAME in "${NAME" and 
+ * term containing the character at that point, parse the rest
+ * of the environment var string (including the final right curly bracket).
+ * Write evar to xbuf, performing any specified text replacements.
+ * Return the new value of e to point just after the final right curly bracket.
+ */
+static int add_evar(struct xbuffer *xbuf, char *buf, int len, int e, constant char *evar, char term)
+{
+	struct replace *replaces = make_replaces(buf, len, &e, term);
+	int v;
+
+	for (v = 0;  evar[v] != '\0'; )
+	{
+		char *repl = find_replace(replaces, evar, &v);
+		if (repl == NULL)
+			xbuf_add_byte(xbuf, evar[v++]);
+		else
+		{
+			int r;
+			for (r = 0;  repl[r] != '\0';  r++)
+			{
+				if (repl[r] == '\\') ++r;
+				xbuf_add_byte(xbuf, repl[r]);
+			}
+		}
+	}
+	free_replaces(replaces);
+	return e;
+}
+
+/*
+ * Expand env variables in a string.
+ */
+public void expand_evars(char *buf, int len, struct xbuffer *xbuf)
+{
+	int i;
+	for (i = 0;  i < len; )
+	{
+		if (i+1 < len && buf[i] == '$' && buf[i+1] == '{')
+		{
+			constant char *evar;
+			char term;
+			int e;
+			i += 2; /* skip "${" */
+			for (e = i;  e < len;  e++)
+				if (buf[e] == '\0' || buf[e] == '}' || buf[e] == '/')
+					break;
+			if (e >= len || buf[e] == '\0')
+				break; /* missing right curly bracket; ignore var */
+			term = buf[e];
+			buf[e++] = '\0';
+			evar = lgetenv_ext(&buf[i], xbuf_char_data(xbuf), xbuf->end);
+			if (evar == NULL) evar = "";
+			i = add_evar(xbuf, buf, len, e, evar, term);
+		} else
+		{
+			xbuf_add_byte(xbuf, buf[i++]);
+		}
+	}
+}

less.nro.VER
@@ -846,6 +846,7 @@ if a lesskey file is found in a standard place (see KEY BINDINGS),
 it is also used as a
 .B lesskey
 file.
+Note the warning under "\-\-lesskey-content" below.
 .IP "\-\-lesskey-src=\fIfilename\fP"
 Causes
 .B less
@@ -871,6 +872,7 @@ Newer versions of
 read the
 .I "lesskey source"
 file directly and ignore the binary file if the source file exists.
+Note the warning under "\-\-lesskey-content" below.
 .IP "\-\-lesskey-content=\fItext\fP"
 Causes less to interpret the specified text as the contents of a
 .BR lesskey (1)
@@ -879,6 +881,17 @@ In the text,
 .B lesskey
 lines may be separated by either newlines as usual, or by semicolons.
 A literal semicolon may be represented by a backslash followed by a semicolon.
+.sp
+Warning: certain environment variables such as 
+LESS, LESSSECURE, LESSCHARSET and others,
+which are used early in startup, 
+cannot be set in a file specified by a command line option
+(\-\-lesskey, \-\-lesskey-src or \-\-lesskey-content).  When using a
+.B lesskey
+file to set environment variables, it is safer to use the
+default lesskey file, or to specify the file using the
+LESSKEYIN or LESSKEY_CONTENT environment variables rather than using 
+a command line option.
 .IP "\-K or \-\-quit-on-intr"
 Causes
 .B less
@@ -2256,6 +2269,8 @@ file.
 Name of the default
 .I "lesskey binary"
 file. (Not used if "$LESSKEYIN" exists.)
+.IP LESSKEY_CONTENT
+The value is parsed as if it were the parameter of a \-\-lesskey-content option.
 .IP LESSKEYIN_SYSTEM
 Name of the default system-wide
 .I "lesskey source"

lesskey.nro.VER
@@ -367,6 +367,24 @@ it should be appended to the end of the preceding line.
 (It cannot be added to the beginning of the += string because space after
 the equals sign is ignored, as noted above.)
 .
+.sp
+In the string after the = sign, a substring of the form ${NAME}
+is replaced with the value of the environment variable "NAME".
+The value of the variable may come from either the system environment,
+an earlier lesskey file, or an earlier definition in the current lesskey file.
+Simple text replacements can be performed by using 
+the syntax ${NAME/STRING/REPL}.
+This replaces all instances of "STRING" in the named 
+environment variable with the text "REPL".
+STRING is matched using a simple text comparison;
+no metacharacters are supported.
+An instance of slash or right curly bracket in STRING or REPL
+must be escaped by preceding it with \fItwo\fP backslashes.
+If REPL is an empty string, all instances of STRING are removed.
+A slash immediately before the right curly bracket may be omitted.
+Multiple replacements may be performed by using
+the syntax ${NAME/STRING1/REPL1/STRING2/REPL2} and so on.
+.
 .SH CONDITIONAL CONFIGURATION
 If a line begins with #version followed by a relational operator and a version number,
 the remainder of the line is parsed if and only if the running version of

main.c
@@ -278,7 +278,6 @@ int main(int argc, char *argv[])
 	init_mark();
 	init_cmds();
 	init_poll();
-	get_term();
 	init_charset();
 	init_line();
 	init_cmdhist();
@@ -321,6 +320,7 @@ int main(int argc, char *argv[])
 	}
 
 	expand_cmd_tables();
+	get_term();
 
 #if EDITOR
 	editor = lgetenv("VISUAL");

version.c
@@ -999,7 +999,8 @@ v644  9/16/23   Improve ^C on non-terminated pipe; fix crash when files are
                 add --match-shift.
 v645            Default Windows charset is utf-8; update Unicode tables;
                 fix ESC-} bug; mouse right-click jumps to '#'; 
-                add LESSSECURE_ALLOW; add --lesskey-content.
+                add LESSSECURE_ALLOW; add --lesskey-content & LESSKEY_CONTENT;
+				allow env var expansion in lesskey files.
 */
 
 char version[] = "645x";

commit ee761f6a6e6b5b936d814878e0c9996407b07a69

Fix bug when one command or environment variable is a prefix of another. This bug was introduced in 09377f769d2f28f0892f152fd57260b9bd240eb6.

charset.c
@@ -716,7 +716,7 @@ public LWCHAR get_wchar(constant char *sp)
 /*
  * Store a character into a UTF-8 string.
  */
-public void put_wchar(char **pp, LWCHAR ch)
+public void put_wchar(mutable char **pp, LWCHAR ch)
 {
 	if (!utf_mode || ch < 0x80) 
 	{

cmdbuf.c
@@ -1284,7 +1284,7 @@ public int cmd_char(char c)
 /*
  * Return the number currently in the command buffer.
  */
-public LINENUM cmd_int(long *frac)
+public LINENUM cmd_int(mutable long *frac)
 {
 	constant char *p;
 	LINENUM n = 0;

cvt.c
@@ -50,7 +50,7 @@ public int * cvt_alloc_chpos(size_t len)
  * Returns converted text in odst.  The original offset of each
  * odst character (when it was in osrc) is returned in the chpos array.
  */
-public void cvt_text(char *odst, constant char *osrc, int *chpos, size_t *lenp, int ops)
+public void cvt_text(mutable char *odst, constant char *osrc, mutable int *chpos, mutable size_t *lenp, int ops)
 {
 	char *dst;
 	char *edst = odst;

decode.c
@@ -694,7 +694,8 @@ static int cmd_search(constant char *cmd, constant char *table, constant char *e
 			 * but not the end of the string in the command table.
 			 * The user's command is incomplete.
 			 */
-			return (A_PREFIX);
+			if (a == A_INVALID)
+				a = A_PREFIX;
 		} else
 		{
 			/*

screen.c
@@ -2478,7 +2478,7 @@ static int parse_color6(constant char **ps)
  *  CV_4BIT: fg/bg values are OR of CV_{RGB} bits.
  *  CV_6BIT: fg/bg values are integers entered by user.
  */
-public COLOR_TYPE parse_color(constant char *str, int *p_fg, int *p_bg)
+public COLOR_TYPE parse_color(constant char *str, mutable int *p_fg, mutable int *p_bg)
 {
 	int fg;
 	int bg;

search.c
@@ -1783,7 +1783,7 @@ public void osc8_search(int search_type, constant char *param, int matches)
 	osc8_search_param = NULL;
 	if (match != 0)
 	{
-		error("OSC8 link not found", NULL_PARG);
+		error("OSC 8 link not found", NULL_PARG);
 		return;
 	}
 	jump_loc(pos, jump_sline);