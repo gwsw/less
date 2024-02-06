a055278b98d406176ae4c21f92e019e1802687e0
Avi Halachmi (:avih) <avihpit@yahoo.com>
2024-02-06 13:38:05 +0200

    windows: improve UTF-8 locale comment (no-op)
    Commit ad440b6 added support for runtime UTF-8 locale, but incorrectly
    documented that it's quite the same as a UTF-8 manifest, except
    startup values which don't automatically become UTF-8 (like argv).
    
    However, that's not quite true.
    
    While the UTF-8 manifest affects all the A(NSI) Windows APIs, like
    CreateFileA and fopen, the runtime setlocale method affects only
    the libc/crt API, and so fopen is covered, but CreateFileA is not.
    
    Fix the comment to mention that.
    
    Currently "less" uses these non-libc ANSI Windows API:
    - CreateFileA: only used with the ASCII name "CONIN$", so OK..
    - FillConsoleOutputCharacterA: only used with spaces and other ASCII.
    - ScrollConsoleScreenBufferA: same as above.
    - WriteConsoleA: only used when the console CP is UTF8, so OK.
    - {Get,Set}ConsoleTitleA: saves/restores the original title, and uses
      console CP regardless of manifest/locale. Will be fixed later.

--- main.c
+++ main.c
@@ -88,7 +88,9 @@ static char *utf8_from_wide(constant wchar_t *ws)
 /*
  * similar to using UTF8 manifest to make the ANSI APIs UTF8, but dynamically
  * with setlocale. unlike the manifest, argv and environ are already ACP, so
- * make them UTF8. CP_ACP remains the original codepage - use less_acp instead.
+ * make them UTF8. Additionally, this affects only the libc/crt API, and so
+ * e.g. fopen filename becomes UTF-8, but CreateFileA filename remains CP_ACP.
+ * CP_ACP remains the original codepage - use the dynamic less_acp instead.
  * effective on win 10 1803 or later when compiled with ucrt, else no-op.
  */
 static void try_utf8_locale(int *pargc, constant char ***pargv)
