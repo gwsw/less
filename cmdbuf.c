gwsw/less

commit 2d0abe8adc5020c0dd596a45b3d7f14902158953

Fix some more type issues.

cmdbuf.c
@@ -31,7 +31,8 @@ static int prompt_col;           /* Column of cursor just after prompt */
 static char *cp;                 /* Pointer into cmdbuf */
 static int cmd_offset;           /* Index into cmdbuf of first displayed char */
 static int literal;              /* Next input char should not be interpreted */
-public ssize_t updown_match = -1;    /* Prefix length in up/down movement */
+public size_t updown_match;      /* Prefix length in up/down movement */
+public lbool have_updown_match = FALSE;
 
 #if TAB_COMPLETE_FILENAME
 static int cmd_complete(int action);
@@ -124,7 +125,7 @@ public void cmd_reset(void)
 	cmd_offset = 0;
 	literal = 0;
 	cmd_mbc_buf_len = 0;
-	updown_match = -1;
+	have_updown_match = FALSE;
 }
 
 /*
@@ -134,7 +135,7 @@ public void clear_cmd(void)
 {
 	cmd_col = prompt_col = 0;
 	cmd_mbc_buf_len = 0;
-	updown_match = -1;
+	have_updown_match = FALSE;
 }
 
 /*
@@ -193,7 +194,7 @@ static constant char * cmd_step_common(char *p, LWCHAR ch, size_t len, int *pwid
 
 	if (len == 1)
 	{
-		pr = prchar((int) ch);
+		pr = prchar(ch);
 		width = (int) strlen(pr);
 	} else
 	{
@@ -460,7 +461,7 @@ static int cmd_ichar(constant char *cs, size_t clen)
 	/*
 	 * Reprint the tail of the line from the inserted char.
 	 */
-	updown_match = -1;
+	have_updown_match = FALSE;
 	cmd_repaint(cp);
 	cmd_right();
 	return (CC_OK);
@@ -503,7 +504,7 @@ static int cmd_erase(void)
 	/*
 	 * Repaint the buffer after the erased char.
 	 */
-	updown_match = -1;
+	have_updown_match = FALSE;
 	cmd_repaint(cp);
 	
 	/*
@@ -596,7 +597,7 @@ static int cmd_kill(void)
 	cmd_offset = 0;
 	cmd_home();
 	*cp = '\0';
-	updown_match = -1;
+	have_updown_match = FALSE;
 	cmd_repaint(cp);
 
 	/*
@@ -643,8 +644,11 @@ static int cmd_updown(int action)
 		return (CC_OK);
 	}
 
-	if (updown_match < 0)
+	if (!have_updown_match)
+	{
 		updown_match = ptr_diff(cp, cmdbuf);
+		have_updown_match = TRUE;
+	}
 
 	/*
 	 * Find the next history entry which matches.

command.c
@@ -920,8 +920,9 @@ public void dispversion(void)
 /*
  * Return a character to complete a partial command, if possible.
  */
-static LWCHAR getcc_end_command(void)
+static char getcc_end_command(void)
 {
+	int ch;
 	switch (mca)
 	{
 	case A_DIGIT:
@@ -934,7 +935,11 @@ static LWCHAR getcc_end_command(void)
 		return ('\n'); 
 	default:
 		/* Some other incomplete command.  Let user complete it. */
-		return ((ungot == NULL) ? getchr() : 0);
+		if (ungot != NULL)
+			return ('\0');
+		ch = getchr();
+		if (ch < 0) ch = '\0';
+		return (char) ch;
 	}
 }
 
@@ -969,7 +974,7 @@ public void getcc_clear(void)
  */
 static char getccu(void)
 {
-	char c = 0;
+	int c = 0;
 	while (c == 0)
 	{
 		if (ungot == NULL)
@@ -977,6 +982,7 @@ static char getccu(void)
 			/* Normal case: no ungotten chars.
 			 * Get char from the user. */
 			c = getchr();
+			if (c < 0) return ('\0');
 		} else
 		{
 			/* Ungotten chars available:
@@ -998,7 +1004,7 @@ static char getcc_repl(char constant *orig, char constant *repl, char (*gr_getc)
 {
 	char c;
 	char keys[16];
-	int ki = 0;
+	size_t ki = 0;
 
 	c = (*gr_getc)();
 	if (orig == NULL || orig[0] == '\0')
@@ -1033,7 +1039,7 @@ static char getcc_repl(char constant *orig, char constant *repl, char (*gr_getc)
 /*
  * Get command character.
  */
-public LWCHAR getcc(void)
+public char getcc(void)
 {
 	/* Replace kent (keypad Enter) with a newline. */
 	return getcc_repl(kent, "\n", getccu, ungetcc);

ch.c
@@ -122,7 +122,8 @@ struct filestate {
 	thisfile->hashtbl[h].hnext = (bn);
 
 static struct filestate *thisfile;
-static int ch_ungotchar = -1;
+static unsigned char ch_ungotchar;
+static lbool ch_have_ungotchar = FALSE;
 static int maxbufs = -1;
 
 extern int autobuf;
@@ -260,11 +261,11 @@ static int ch_get(void)
 		 * If we read less than a full block, that's ok.
 		 * We use partial block and pick up the rest next time.
 		 */
-		if (ch_ungotchar != -1)
+		if (ch_have_ungotchar)
 		{
 			bp->data[bp->datasize] = ch_ungotchar;
 			n = 1;
-			ch_ungotchar = -1;
+			ch_have_ungotchar = FALSE;
 		} else if (ch_flags & CH_HELPFILE)
 		{
 			bp->data[bp->datasize] = helpdata[ch_fpos];
@@ -372,9 +373,15 @@ static int ch_get(void)
  */
 public void ch_ungetchar(int c)
 {
-	if (c != -1 && ch_ungotchar != -1)
-		error("ch_ungetchar overrun", NULL_PARG);
-	ch_ungotchar = c;
+	if (c < 0)
+		ch_have_ungotchar = FALSE;
+	else
+	{
+		if (ch_have_ungotchar)
+			error("ch_ungetchar overrun", NULL_PARG);
+		ch_ungotchar = (unsigned char) c;
+		ch_have_ungotchar = TRUE;
+	}
 }
 
 #if LOGFILE

charset.c
@@ -521,9 +521,10 @@ public int binary_char(LWCHAR c)
 /*
  * Is a given character a "control" character?
  */
-public int control_char(LWCHAR c)
+public lbool control_char(LWCHAR c)
 {
-	c &= 0377;
+	if (!is_ascii_char(c))
+		return FALSE;
 	return (chardef[c] & IS_CONTROL_CHAR);
 }
 
@@ -536,7 +537,7 @@ public constant char * prchar(LWCHAR c)
 	/* {{ This buffer can be overrun if LESSBINFMT is a long string. }} */
 	static char buf[MAX_PRCHAR_LEN+1];
 
-	c &= 0377;
+	c &= 0377; /*{{type-issue}}*/
 	if ((c < 128 || !utf_mode) && !control_char(c))
 		SNPRINTF1(buf, sizeof(buf), "%c", (int) c);
 	else if (c == ESC)
@@ -637,8 +638,7 @@ public lbool is_utf8_well_formed(constant char *ss, int slen)
 			return (FALSE);
 	} else
 	{
-		unsigned char mask;
-		mask = (~((1 << (8-len)) - 1)) & 0xFF;
+		unsigned char mask = (~((((unsigned char)1) << (8-len)) - 1)) & 0xFF;
 		if (s[0] == mask && (s[1] & mask) == 0x80)
 			return (FALSE);
 	}

cvt.c
@@ -82,7 +82,7 @@ public void cvt_text(char *odst, constant char *osrc, int *chpos, size_t *lenp, 
 			{
 				if (ansi_step(pansi, ch) != ANSI_MID)
 					break;
-				ch = *src++;
+				ch = (LWCHAR) *src++; /* {{ would step_char work? }} */
 			}
 			ansi_done(pansi);
 		} else

decode.c
@@ -281,7 +281,7 @@ static void expand_special_keys(unsigned char *table, size_t len)
 			if (repl == NULL || strlen(repl) > klen)
 				repl = "\377";
 			while (*repl != '\0')
-				*to++ = *repl++;
+				*to++ = (unsigned char) *repl++; /*{{type-issue}}*/
 		}
 		*to++ = '\0';
 		/*

evar.c
@@ -144,14 +144,14 @@ static size_t add_evar(struct xbuffer *xbuf, mutable char *buf, size_t len, size
 	{
 		constant char *repl = find_replace(replaces, evar, &v);
 		if (repl == NULL)
-			xbuf_add_byte(xbuf, evar[v++]);
+			xbuf_add_char(xbuf, evar[v++]);
 		else
 		{
 			size_t r;
 			for (r = 0;  repl[r] != '\0';  r++)
 			{
 				if (repl[r] == '\\') ++r;
-				xbuf_add_byte(xbuf, repl[r]);
+				xbuf_add_char(xbuf, repl[r]);
 			}
 		}
 	}
@@ -186,7 +186,7 @@ public void expand_evars(mutable char *buf, size_t len, struct xbuffer *xbuf)
 			i = add_evar(xbuf, buf, len, e, evar, term);
 		} else
 		{
-			xbuf_add_byte(xbuf, buf[i++]);
+			xbuf_add_char(xbuf, buf[i++]);
 		}
 	}
 }

filename.c
@@ -514,7 +514,7 @@ static char * readfd(FILE *fd)
 		int ch;
 		if ((ch = getc(fd)) == '\n' || ch == EOF)
 			break;
-		xbuf_add_char(&xbuf, ch);
+		xbuf_add_char(&xbuf, (char) ch);
 	}
 	xbuf_add_char(&xbuf, '\0');
 	return (char *) xbuf.data;

commit 855ab5cc9b5aed722a0a295c067c3d51ccf8c48f

Fix bug in doing a save/restore of updown_match. Previously this was done by saving the value of updown_match, but recently updown_match was split into two varibles. Need to save/restore have_updown_match as well.
This is yet another bug caused by a change in
2d0abe8adc5020c0dd596a45b3d7f14902158953.

cmdbuf.c
@@ -688,7 +688,23 @@ static int cmd_updown(int action)
 	bell();
 	return (CC_OK);
 }
-#endif
+
+/*
+ * Yet another lesson in the evils of global variables.
+ */
+public ssize_t save_updown_match(void)
+{
+	if (!have_updown_match)
+		return (ssize_t)(-1);
+	return (ssize_t) updown_match;
+}
+
+public void restore_updown_match(ssize_t udm)
+{
+	updown_match = udm;
+	have_updown_match = (udm != (ssize_t)(-1));
+}
+#endif /* CMD_HISTORY */
 
 /*
  *

command.c
@@ -46,7 +46,6 @@ extern void *ml_search;
 extern void *ml_examine;
 extern int wheel_lines;
 extern int def_search_type;
-extern int updown_match;
 extern lbool search_wrapped;
 #if SHELL_ESCAPE || PIPEC
 extern void *ml_shell;
@@ -726,7 +725,7 @@ static int mca_char(char c)
 		{
 			/* Incremental search: do a search after every input char. */
 			int st = (search_type & (SRCH_FORW|SRCH_BACK|SRCH_NO_MATCH|SRCH_NO_REGEX|SRCH_NO_MOVE|SRCH_WRAP|SRCH_SUBSEARCH_ALL));
-			int save_updown_match = updown_match;
+			ssize_t save_updown;
 			constant char *pattern = get_cmdbuf();
 			if (pattern == NULL)
 				return (MCA_MORE);
@@ -735,6 +734,7 @@ static int mca_char(char c)
 			 * reinits it. That breaks history scrolling.
 			 * {{ This is ugly. mca_search probably shouldn't call set_mlist. }}
 			 */
+			save_updown = save_updown_match();
 			cmd_exec();
 			if (*pattern == '\0')
 			{
@@ -753,7 +753,7 @@ static int mca_char(char c)
 				repaint();
 			}
 			mca_search1();
-			updown_match = save_updown_match;
+			restore_updown_match(save_updown);
 			cmd_repaint(NULL);
 		}
 		break;