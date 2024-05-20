gwsw/less

commit 2a642a07d86f7f9484db18cd748bc521e71c997f

Fix incorrect display when filename contains control chars. Such chars should not be printed directly to the screen, but instead passed through prchar() or prutfchar().

prompt.c
@@ -28,6 +28,7 @@ extern int sc_height;
 extern int jump_sline;
 extern int less_is_more;
 extern int header_lines;
+extern int utf_mode;
 extern IFILE curr_ifile;
 #if OSC8_LINK
 extern char *osc8_path;
@@ -82,11 +83,17 @@ public void init_prompt(void)
  */
 static void ap_str(constant char *s)
 {
-	size_t len = strlen(s);
-	if (mp + len >= message + PROMPT_SIZE)
-		len = ptr_diff(message, mp) + PROMPT_SIZE - 1;
-	strncpy(mp, s, len);
-	mp += len;
+	constant char *es = s + strlen(s);
+	while (*s != '\0')
+	{
+		LWCHAR ch = step_charc(&s, +1, es);
+		constant char *ps = utf_mode ? prutfchar(ch) : prchar(ch);
+		size_t plen = strlen(ps);
+		if (mp + plen >= message + PROMPT_SIZE)
+			break;
+		strcpy(mp, ps);
+		mp += plen;
+	}
 	*mp = '\0';
 }
 

output.c
@@ -30,6 +30,7 @@ extern int sc_width;
 extern int so_s_width, so_e_width;
 extern int is_tty;
 extern int oldbot;
+extern int utf_mode;
 extern char intr_char;
 
 #if MSDOS_COMPILER==WIN32C || MSDOS_COMPILER==BORLANDC || MSDOS_COMPILER==DJGPPC
@@ -550,6 +551,7 @@ IPRINT_FUNC(iprint_linenum, LINENUM, linenumtoa)
 public int less_printf(constant char *fmt, PARG *parg)
 {
 	constant char *s;
+	constant char *es;
 	int col;
 
 	col = 0;
@@ -566,11 +568,17 @@ public int less_printf(constant char *fmt, PARG *parg)
 			{
 			case 's':
 				s = parg->p_string;
+				es = s + strlen(s);
 				parg++;
 				while (*s != '\0')
 				{
-					putchr(*s++);
-					col++;
+					LWCHAR ch = step_charc(&s, +1, es);
+					constant char *ps = utf_mode ? prutfchar(ch) : prchar(ch);
+					while (*ps != '\0')
+					{
+						putchr(*ps++);
+						col++;
+					}
 				}
 				break;
 			case 'd':

commit bf14bc4a61e59674cfd4d93a9f2cfcb902a5951d

Fix bug using escape sequences in prompts. Change 2a642a07d86f7f9484db18cd748bc521e71c997f introduced a regression where a prompt set by -P is converted to printable form before printing. This breaks using escape sequences to affect the formatting of prompt strings.
Now only filenames in a prompt are converted to printable form,
rather than the entire prompt (previously implemented
in 2cdb98461a4e7ccc0732c85cacbe84e2da1eb4d7).

related to #515

prompt.c
@@ -80,15 +80,30 @@ public void init_prompt(void)
 
 /*
  * Append a string to the end of the message.
+ * nprt means the character *may* be nonprintable
+ * and should be converted to printable form.
  */
-static void ap_str(constant char *s)
+static void ap_estr(constant char *s, lbool nprt)
 {
 	constant char *es = s + strlen(s);
 	while (*s != '\0')
 	{
 		LWCHAR ch = step_charc(&s, +1, es);
-		constant char *ps = utf_mode ? prutfchar(ch) : prchar(ch);
-		size_t plen = strlen(ps);
+		constant char *ps;
+		char ubuf[MAX_UTF_CHAR_LEN+1];
+		size_t plen;
+
+		if (nprt)
+		{
+			ps = utf_mode ? prutfchar(ch) : prchar(ch);
+		} else
+		{
+			char *up = ubuf;
+			put_wchar(&up, ch);
+			*up = '\0';
+			ps = ubuf;
+		}
+		plen = strlen(ps);
 		if (mp + plen >= message + PROMPT_SIZE)
 			break;
 		strcpy(mp, ps);
@@ -96,6 +111,12 @@ static void ap_str(constant char *s)
 	}
 	*mp = '\0';
 }
+
+static void ap_str(constant char *s)
+{
+	ap_estr(s, FALSE);
+}
+
 
 /*
  * Append a character to the end of the message.
@@ -289,10 +310,10 @@ static void protochar(char c, int where)
 		break;
 #endif
 	case 'f': /* File name */
-		ap_str(get_filename(curr_ifile));
+		ap_estr(get_filename(curr_ifile), TRUE);
 		break;
 	case 'F': /* Last component of file name */
-		ap_str(last_component(get_filename(curr_ifile)));
+		ap_estr(last_component(get_filename(curr_ifile)), TRUE);
 		break;
 	case 'g': /* Shell-escaped file name */
 		s = shell_quote(get_filename(curr_ifile));