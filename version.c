d044a2a9dfca08ea473a31bd50da657c23e83aff
Mark Nudelman <markn@greenwoodsoftware.com>
2024-08-25 22:45:28 -0700

    Fix Windows build.
    There was a conflict between the definition of BOOL in option.h
    and a BOOL typedef in a Windows header file. Move the #include of
    option.h after the #include of the Windows headers.
    This is rather ugly. We should probably rename the #defines in
    option.h to avoid such conflicts.
    Related to #561.

--- version.c
+++ version.c
@@ -1027,7 +1027,7 @@ v662  8/8/24    Fix build with --with-secure; improve true colors on Windows;
                 fix --shift with fractional parameter; fix EOF bug in R command;
                 fix --header with short file; fix ^X bug when output is not tty.
 v663  8/16/24   Fix ^X bug when output is not a tty.
-v664
+v664            Fix Windows compile error.
 */
 
 char version[] = "664x";
