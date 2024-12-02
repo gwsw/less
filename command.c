gwsw/less

commit 7e6da25e345b146e2ced95a705dfe865d8c650cc

Fix bug with multibyte UTF-8 chars in search string when --incsearch is in effect.

command.c
@@ -242,6 +242,8 @@ exec_mca(VOID_PARAM)
 
 	cmd_exec();
 	cbuf = get_cmdbuf();
+	if (cbuf == NULL)
+		return;
 
 	switch (mca)
 	{
@@ -423,6 +425,8 @@ mca_opt_nonfirst_char(c)
 	if (cmd_char(c) == CC_QUIT)
 		return (MCA_DONE);
 	p = get_cmdbuf();
+	if (p == NULL)
+		return (MCA_MORE);
 	opt_lower = ASCII_IS_LOWER(p[0]);
 	err = 0;
 	curropt = findopt_name(&p, &oname, &err);
@@ -478,6 +482,8 @@ mca_opt_char(c)
 		if (curropt == NULL)
 		{
 			parg.p_string = get_cmdbuf();
+			if (parg.p_string == NULL)
+				return (MCA_MORE);
 			error("There is no --%s option", &parg);
 			return (MCA_DONE);
 		}
@@ -702,6 +708,8 @@ mca_char(c)
 			/* Incremental search: do a search after every input char. */
 			int st = (search_type & (SRCH_FORW|SRCH_BACK|SRCH_NO_MATCH|SRCH_NO_REGEX|SRCH_NO_MOVE|SRCH_WRAP));
 			char *pattern = get_cmdbuf();
+			if (pattern == NULL)
+				return (MCA_MORE);
 			/*
 			 * Must save updown_match because mca_search
 			 * reinits it. That breaks history scrolling.
@@ -1308,6 +1316,8 @@ commands(VOID_PARAM)
 				if (cmd_char(c) == CC_QUIT || len_cmdbuf() == 0)
 					continue;
 				cbuf = get_cmdbuf();
+				if (cbuf == NULL)
+					continue;
 			} else
 			{
 				/*

cmdbuf.c
@@ -1378,6 +1378,9 @@ cmd_int(frac)
 	public char *
 get_cmdbuf(VOID_PARAM)
 {
+	if (cmd_mbc_buf_index < cmd_mbc_buf_len)
+		/* Don't return buffer containing an incomplete multibyte char. */
+		return (NULL);
 	return (cmdbuf);
 }
 

commit deb9a06e16ae1afac4bda8d9199a32df162b50bd

Fix use of multibyte characters as commands.
When we process the first byte(s) of a multibyte command char,
we should just accept it and get the next byte, without changing
state. The previous code was calling cmd_reset(), which discarded
any previous chars in the cmd buffer.

This was probably broken in 7e6da25e345b146e2ced95a705dfe865d8c650cc.

Related to #595.

command.c
@@ -1448,7 +1448,10 @@ public void commands(void)
 					continue;
 				cbuf = get_cmdbuf();
 				if (cbuf == NULL)
-					continue;
+				{
+					c = getcc();
+					goto again;
+				}
 				action = fcmd_decode(cbuf, &extra);
 			} else
 			{

version.c
@@ -1039,7 +1039,7 @@ v669  11/6/24   Add --no-paste; add --no-edit-warn; add TAB for -- command;
                 add form-feed; add --no-poll; add LESS_SIGUSR1;
                 fix "configure --with-secure=no".
 v670  11/16/24  Fix SIGWINCH handling; add iopen.
-v671
+v671            Fix multibyte command chars.
 */
 
 char version[] = "671x";