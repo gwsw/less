gwsw/less

commit da2a9ecdf16beb642d0c030e35f0351c5f2e5a12

New strategy for handling highlighting.
Previously, prep_hilite was called in several places and would try to
prepare a largish section of the file at once. The size of the prepped
section was more or less random, depending on the size of the line buffer
at the time of the call. This was ostensibly done for "efficiency", but
it's not really much more efficient than prepping smaller sections in
several calls to prep_hilite. It has the negative side effect that
when file input was not immediately available (eg. a slow pipe), less
would get stuck in prep_hilite waiting for data, and not display data
that was already available.

This commit changes the strategy so that we generally prep only one line at
a time. This avoids unnecessarily waiting for more data before displaying
data that is already available. There are also some changes specifically
to improve the behavior of the & command, especially when dealing with
slow input.

Related to #532 and others.

forwback.c
@@ -40,7 +40,6 @@ extern int stop_on_form_feed;
 extern POSITION header_start_pos;
 extern lbool no_poll;
 #if HILITE_SEARCH
-extern size_t size_linebuf;
 extern int hilite_search;
 extern int status_col;
 #endif
@@ -139,7 +138,7 @@ static POSITION forw_line_pfx(POSITION pos, int pfx, int skipeol)
 	sc_width = pfx + line_pfx_width();
 	auto_wrap = 0;
 	hshift = 0;
-	pos = forw_line_seg(pos, skipeol, FALSE, FALSE, NULL);
+	pos = forw_line_seg(pos, skipeol, FALSE, FALSE, NULL, NULL);
 	sc_width = save_sc_width;
 	auto_wrap = save_auto_wrap;
 	hshift = save_hshift;
@@ -174,7 +173,7 @@ public int overlay_header(void)
 		home();
 		for (ln = 0; ln < header_lines; ++ln)
 		{
-			pos = forw_line(pos, NULL);
+			pos = forw_line(pos, NULL, NULL);
 			set_attr_header(ln);
 			clear_eol();
 			put_line(FALSE);
@@ -222,6 +221,7 @@ public void forw(int n, POSITION pos, lbool force, lbool only_last, lbool to_new
 	int nlines = 0;
 	lbool do_repaint;
 	lbool newline;
+	lbool new_screen = FALSE;
 
 	if (pos != NULL_POSITION)
 		pos = after_header_pos(pos);
@@ -238,14 +238,6 @@ public void forw(int n, POSITION pos, lbool force, lbool only_last, lbool to_new
 	 */
 	do_repaint = (only_last && n > sc_height-1) || 
 		(forw_scroll >= 0 && n > forw_scroll && n != sc_height-1);
-
-#if HILITE_SEARCH
-	if (pos != NULL_POSITION && (hilite_search == OPT_ONPLUS || is_filtering() || status_col)) {
-		prep_hilite(pos, pos + (POSITION) (4*size_linebuf), ignore_eoi ? 1 : -1);
-		pos = next_unfiltered(pos);
-	}
-#endif
-
 	if (!do_repaint)
 	{
 		if (top_scroll && n >= sc_height - 1 && pos != ch_length())
@@ -256,8 +248,8 @@ public void forw(int n, POSITION pos, lbool force, lbool only_last, lbool to_new
 			 *    to hit eof in the middle of this screen,
 			 *    but we don't yet know if that will happen. }}
 			 */
+			new_screen = TRUE;
 			pos_clear();
-			add_forw_pos(pos);
 			force = TRUE;
 			clear();
 			home();
@@ -270,8 +262,8 @@ public void forw(int n, POSITION pos, lbool force, lbool only_last, lbool to_new
 			 * currently displayed.  Clear the screen image 
 			 * (position table) and start a new screen.
 			 */
+			new_screen = TRUE;
 			pos_clear();
-			add_forw_pos(pos);
 			force = TRUE;
 			if (top_scroll)
 			{
@@ -286,6 +278,7 @@ public void forw(int n, POSITION pos, lbool force, lbool only_last, lbool to_new
 
 	while (--n >= 0)
 	{
+		POSITION linepos;
 		/*
 		 * Read the next line of input.
 		 */
@@ -304,12 +297,9 @@ public void forw(int n, POSITION pos, lbool force, lbool only_last, lbool to_new
 			/* 
 			 * Get the next line from the file.
 			 */
-			pos = forw_line(pos, &newline);
+			pos = forw_line(pos, &linepos, &newline);
 			if (to_newline && !newline)
 				++n;
-#if HILITE_SEARCH
-			pos = next_unfiltered(pos);
-#endif
 			if (pos == NULL_POSITION)
 			{
 				/*
@@ -330,6 +320,11 @@ public void forw(int n, POSITION pos, lbool force, lbool only_last, lbool to_new
 		 * Add the position of the next line to the position table.
 		 * Display the current line on the screen.
 		 */
+		if (new_screen)
+		{
+			add_forw_pos(linepos);
+			new_screen = FALSE;
+		}
 		add_forw_pos(pos);
 		nlines++;
 		if (do_repaint)
@@ -357,27 +352,6 @@ public void forw(int n, POSITION pos, lbool force, lbool only_last, lbool to_new
 		put_line(TRUE);
 		if (stop_on_form_feed && !do_repaint && line_is_ff() && position(TOP) != NULL_POSITION)
 			break;
-#if 0
-		/* {{ 
-		 * Can't call clear_eol here.  The cursor might be at end of line
-		 * on an ignaw terminal, so clear_eol would clear the last char
-		 * of the current line instead of all of the next line.
-		 * If we really need to do this on clear_bg terminals, we need
-		 * to find a better way.
-		 * }}
-		 */
-		if (clear_bg && apply_at_specials(final_attr) != AT_NORMAL)
-		{
-			/*
-			 * Writing the last character on the last line
-			 * of the display may have scrolled the screen.
-			 * If we were in standout mode, clear_bg terminals 
-			 * will fill the new line with the standout color.
-			 * Now we're in normal mode again, so clear the line.
-			 */
-			clear_eol();
-		}
-#endif
 		forw_prompt = 1;
 	}
 	if (nlines == 0 && !ignore_eoi)
@@ -401,23 +375,17 @@ public void back(int n, POSITION pos, lbool force, lbool only_last, lbool to_new
 	int nlines = 0;
 	lbool do_repaint;
 	lbool newline;
+	POSITION linepos;
 
 	squish_check();
 	do_repaint = (n > get_back_scroll() || (only_last && n > sc_height-1) || header_lines > 0);
-#if HILITE_SEARCH
-	if (pos != NULL_POSITION && (hilite_search == OPT_ONPLUS || is_filtering() || status_col)) {
-		prep_hilite((pos < (POSITION) (3*size_linebuf)) ? 0 : pos - (POSITION) (3*size_linebuf), pos, -1);
-	}
-#endif
+
 	while (--n >= 0)
 	{
 		/*
 		 * Get the previous line of input.
 		 */
-#if HILITE_SEARCH
-		pos = prev_unfiltered(pos);
-#endif
-		pos = back_line(pos, &newline);
+		pos = back_line(pos, &linepos, &newline);
 		if (to_newline && !newline)
 			++n;
 		if (pos == NULL_POSITION)
@@ -439,7 +407,7 @@ public void back(int n, POSITION pos, lbool force, lbool only_last, lbool to_new
 		 * Add the position of the previous line to the position table.
 		 * Display the line on the screen.
 		 */
-		add_back_pos(pos);
+		add_back_pos(linepos);
 		nlines++;
 		if (!do_repaint)
 		{
@@ -558,7 +526,7 @@ public lbool get_one_screen(void)
 	no_poll = TRUE;
 	for (nlines = 0;  nlines + shell_lines <= sc_height;  nlines++)
 	{
-		pos = forw_line(pos, NULL);
+		pos = forw_line(pos, NULL, NULL);
 		if (pos == NULL_POSITION)
 		{
 			ret = TRUE;

input.c
@@ -29,7 +29,6 @@ extern POSITION start_attnpos;
 extern POSITION end_attnpos;
 #if HILITE_SEARCH
 extern int hilite_search;
-extern size_t size_linebuf;
 extern int show_attn;
 #endif
 
@@ -82,7 +81,7 @@ static void init_status_col(POSITION base_pos, POSITION disp_pos, POSITION edisp
  * a line.  The new position is the position of the first character
  * of the NEXT line.  The line obtained is the line starting at curr_pos.
  */
-public POSITION forw_line_seg(POSITION curr_pos, lbool skipeol, lbool rscroll, lbool nochop, lbool *p_newline)
+public POSITION forw_line_seg(POSITION curr_pos, lbool skipeol, lbool rscroll, lbool nochop, POSITION *p_linepos, lbool *p_newline)
 {
 	POSITION base_pos;
 	POSITION new_pos;
@@ -111,8 +110,7 @@ get_forw_line:
 		 * If we're not ignoring EOI, we *could* do the same, but
 		 * for efficiency we prepare several lines ahead at once.
 		 */
-		prep_hilite(curr_pos, curr_pos + (POSITION) (3*size_linebuf), ignore_eoi ? 1 : -1);
-		curr_pos = next_unfiltered(curr_pos);
+		prep_hilite(curr_pos, NULL_POSITION, 1);
 	}
 #endif
 	if (ch_seek(curr_pos))
@@ -324,14 +322,16 @@ get_forw_line:
 			(void) ch_back_get();
 		new_pos = ch_tell();
 	}
+	if (p_linepos != NULL)
+		*p_linepos = base_pos;
 	if (p_newline != NULL)
 		*p_newline = endline;
 	return (new_pos);
 }
 
-public POSITION forw_line(POSITION curr_pos, lbool *p_newline)
+public POSITION forw_line(POSITION curr_pos, POSITION *p_linepos, lbool *p_newline)
 {
-	return forw_line_seg(curr_pos, (chop_line() || hshift > 0), TRUE, FALSE, p_newline);
+	return forw_line_seg(curr_pos, (chop_line() || hshift > 0), TRUE, FALSE, p_linepos, p_newline);
 }
 
 /*
@@ -341,7 +341,7 @@ public POSITION forw_line(POSITION curr_pos, lbool *p_newline)
  * a line.  The new position is the position of the first character
  * of the PREVIOUS line.  The line obtained is the one starting at new_pos.
  */
-public POSITION back_line(POSITION curr_pos, lbool *p_newline)
+public POSITION back_line(POSITION curr_pos, POSITION *p_linepos, lbool *p_newline)
 {
 	POSITION base_pos;
 	POSITION new_pos;
@@ -360,11 +360,6 @@ get_back_line:
 		null_line();
 		return (NULL_POSITION);
 	}
-#if HILITE_SEARCH
-	if (hilite_search == OPT_ONPLUS || is_filtering() || status_col)
-		prep_hilite((curr_pos < (POSITION) (3*size_linebuf)) ? 0 : 
-		    curr_pos - (POSITION) (3*size_linebuf), curr_pos, -1);
-#endif
 	if (ch_seek(curr_pos-1))
 	{
 		null_line();
@@ -426,6 +421,11 @@ get_back_line:
 			break;
 		}
 	}
+
+#if HILITE_SEARCH
+	if (hilite_search == OPT_ONPLUS || is_filtering() || status_col)
+		prep_hilite(base_pos, NULL_POSITION, 1);
+#endif
 
 	/*
 	 * Now scan forwards from the beginning of this line.
@@ -569,6 +569,8 @@ get_back_line:
 	if (status_col)
 		init_status_col(base_pos, line_position(), edisp_pos, new_pos);
 #endif
+	if (p_linepos != NULL)
+		*p_linepos = base_pos;
 	return (begin_new_pos);
 }
 

jump.c
@@ -53,7 +53,7 @@ public void jump_forw(void)
 	 * to get to the beginning of the last line.
 	 */
 	pos_clear();
-	pos = back_line(end_pos, NULL);
+	pos = back_line(end_pos, NULL, NULL);
 	if (pos == NULL_POSITION)
 		jump_loc(ch_zero(), sc_height-1);
 	else
@@ -293,7 +293,7 @@ public void jump_loc(POSITION pos, int sline)
 #endif
 				return;
 			}
-			pos = back_line(pos, NULL);
+			pos = back_line(pos, NULL, NULL);
 			if (pos == NULL_POSITION)
 			{
 				/*
@@ -319,7 +319,7 @@ public void jump_loc(POSITION pos, int sline)
 		 */
 		for (nline = sindex;  nline < sc_height - 1;  nline++)
 		{
-			pos = forw_line(pos, NULL);
+			pos = forw_line(pos, NULL, NULL);
 			if (pos == NULL_POSITION)
 			{
 				/*
@@ -329,9 +329,6 @@ public void jump_loc(POSITION pos, int sline)
 				 */
 				break;
 			}
-#if HILITE_SEARCH
-			pos = next_unfiltered(pos);
-#endif
 			if (pos >= tpos)
 			{
 				/* 

line.c
@@ -50,7 +50,7 @@ static struct xbuffer last_ansi;
 static struct xbuffer last_ansis[NUM_LAST_ANSIS];
 static int curr_last_ansi;
 
-public size_t size_linebuf = 0; /* Size of line buffer (and attr buffer) */
+static size_t size_linebuf = 0; /* Size of line buffer (and attr buffer) */
 static struct ansi_state *line_ansi = NULL;
 static lbool ansi_in_line;
 static int ff_starts_line;
@@ -1847,7 +1847,7 @@ public int rrshift(void)
 			break;
 	for (; sline < sc_height && pos != NULL_POSITION; sline++)
 	{
-		pos = forw_line(pos, NULL);
+		pos = forw_line(pos, NULL, NULL);
 		if (end_column > longest)
 			longest = end_column;
 	}

search.c
@@ -44,7 +44,6 @@ extern int header_cols;
 extern LWCHAR rscroll_char;
 #if HILITE_SEARCH
 extern int hilite_search;
-extern size_t size_linebuf;
 extern lbool squished;
 extern int can_goto_line;
 extern lbool no_eof_bell;
@@ -289,7 +288,7 @@ public void repaint_hilite(lbool on)
 		pos = position(sindex);
 		if (pos == NULL_POSITION)
 			continue;
-		(void) forw_line(pos, NULL);
+		(void) forw_line(pos, NULL, NULL);
 		goto_line(sindex);
 		clear_eol();
 		put_line(FALSE);
@@ -336,7 +335,7 @@ public void clear_attn(void)
 		if (pos <= old_end_attnpos &&
 		     (epos == NULL_POSITION || epos > old_start_attnpos))
 		{
-			(void) forw_line(pos, NULL);
+			(void) forw_line(pos, NULL, NULL);
 			goto_line(sindex);
 			clear_eol();
 			put_line(FALSE);
@@ -550,11 +549,10 @@ public lbool is_filtered(POSITION pos)
 {
 	struct hilite_node *n;
 
-	if (ch_getflags() & CH_HELPFILE)
+	if (!is_filtering())
 		return (FALSE);
 	if (pos_in_header(pos))
 		return (FALSE);
-
 	n = hlist_find(&filter_anchor, pos);
 	return (n != NULL && pos >= n->r.hl_startpos);
 }
@@ -565,47 +563,19 @@ public lbool is_filtered(POSITION pos)
  */
 public POSITION next_unfiltered(POSITION pos)
 {
-	struct hilite_node *n;
-
-	if (ch_getflags() & CH_HELPFILE)
+	if (!is_filtering())
 		return (pos);
 	if (pos_in_header(pos))
 		return (pos);
-
 	flush();
-	n = hlist_find(&filter_anchor, pos);
-	while (n != NULL && pos >= n->r.hl_startpos)
+	while (pos != NULL_POSITION)
 	{
-		pos = n->r.hl_endpos;
-		n = n->next;
-	}
-	return (pos);
-}
-
-/*
- * If pos is hidden, return the previous position which isn't or 0 if
- * we're filtered right to the beginning, otherwise just return pos.
- */
-public POSITION prev_unfiltered(POSITION pos)
-{
-	struct hilite_node *n;
-
-	if (ch_getflags() & CH_HELPFILE)
-		return (pos);
-	if (pos_in_header(pos))
-		return (pos);
-
-	flush();
-	n = hlist_find(&filter_anchor, pos);
-	while (n != NULL && pos >= n->r.hl_startpos)
-	{
-		pos = n->r.hl_startpos;
-		if (pos == 0)
+		prep_hilite(pos, NULL_POSITION, 1);
+		if (!is_filtered(pos))
 			break;
-		pos--;
-		n = n->prev;
+		pos = forw_raw_line(pos, NULL, NULL);
 	}
-	return (pos);
+	return pos;
 }
 
 /*
@@ -1236,7 +1206,7 @@ static POSITION get_lastlinepos(POSITION pos, POSITION tpos, int sheight)
 	flush();
 	for (nlines = 0;;  nlines++)
 	{
-		POSITION npos = forw_line(pos, NULL);
+		POSITION npos = forw_line(pos, NULL, NULL);
 		if (npos > tpos)
 		{
 			if (nlines < sheight)
@@ -2220,12 +2190,6 @@ public void prep_hilite(POSITION spos, POSITION epos, int maxlines)
 	int result;
 	int i;
 
-	/*
-	 * Search beyond where we're asked to search, so the prep region covers
-	 * more than we need.  Do one big search instead of a bunch of small ones.
-	 */
-	POSITION SEARCH_MORE = (POSITION) (3*size_linebuf);
-
 	if (!prev_pattern(&search_info) && !is_filtering())
 		return;
 
@@ -2247,6 +2211,8 @@ public void prep_hilite(POSITION spos, POSITION epos, int maxlines)
 		for (i = 0;  i < maxlines;  i++)
 			max_epos = forw_raw_line(max_epos, NULL, NULL);
 	}
+	if (epos == NULL_POSITION || (max_epos != NULL_POSITION && epos > max_epos))
+		epos = max_epos;
 
 	/*
 	 * Find two ranges:
@@ -2264,28 +2230,13 @@ public void prep_hilite(POSITION spos, POSITION epos, int maxlines)
 		 */
 		clr_hilite();
 		clr_filter();
-		if (epos != NULL_POSITION)
-			epos += SEARCH_MORE;
 		nprep_startpos = spos;
 	} else
 	{
 		/*
 		 * New range partially or completely overlaps old prep region.
 		 */
-		if (epos == NULL_POSITION)
-		{
-			/*
-			 * New range goes to end of file.
-			 */
-			;
-		} else if (epos > prep_endpos)
-		{
-			/*
-			 * New range ends after old prep region.
-			 * Extend prep region to end at end of new range.
-			 */
-			epos += SEARCH_MORE;
-		} else /* (epos <= prep_endpos) */
+		if (epos != NULL_POSITION && epos <= prep_endpos)
 		{
 			/*
 			 * New range ends within old prep region.
@@ -2293,7 +2244,6 @@ public void prep_hilite(POSITION spos, POSITION epos, int maxlines)
 			 */
 			epos = prep_startpos;
 		}
-
 		if (spos < prep_startpos)
 		{
 			/*
@@ -2301,10 +2251,6 @@ public void prep_hilite(POSITION spos, POSITION epos, int maxlines)
 			 * Extend old prep region backwards to start at 
 			 * start of new range.
 			 */
-			if (spos < SEARCH_MORE)
-				spos = 0;
-			else
-				spos -= SEARCH_MORE;
 			nprep_startpos = spos;
 		} else /* (spos >= prep_startpos) */
 		{
@@ -2315,13 +2261,6 @@ public void prep_hilite(POSITION spos, POSITION epos, int maxlines)
 			spos = prep_endpos;
 		}
 	}
-
-	if (epos != NULL_POSITION && max_epos != NULL_POSITION &&
-	    epos > max_epos)
-		/*
-		 * Don't go past the max position we're allowed.
-		 */
-		epos = max_epos;
 
 	if (epos == NULL_POSITION || epos > spos)
 	{

commit 591911166d41ea896c53d7cc629ff16f0fe72d51

Fix bug scrolling backwards with long lines.
da2a9ecdf16beb642d0c030e35f0351c5f2e5a12 introduced a bug
in back() where it stored into the position table the position
of the start of the previous physical (file) line, rather than
the start of the previous screen line.

forwback.c
@@ -380,7 +380,6 @@ public void back(int n, POSITION pos, lbool force, lbool only_last, lbool to_new
 	int nlines = 0;
 	lbool do_repaint;
 	lbool newline;
-	POSITION linepos;
 
 	squish_check();
 	do_repaint = (n > get_back_scroll() || (only_last && n > sc_height-1) || header_lines > 0);
@@ -390,7 +389,7 @@ public void back(int n, POSITION pos, lbool force, lbool only_last, lbool to_new
 		/*
 		 * Get the previous line of input.
 		 */
-		pos = back_line(pos, &linepos, &newline);
+		pos = back_line(pos, NULL, &newline);
 		if (to_newline && !newline)
 			++n;
 		if (pos == NULL_POSITION)
@@ -412,7 +411,7 @@ public void back(int n, POSITION pos, lbool force, lbool only_last, lbool to_new
 		 * Add the position of the previous line to the position table.
 		 * Display the line on the screen.
 		 */
-		add_back_pos(linepos);
+		add_back_pos(pos);
 		nlines++;
 		if (!do_repaint)
 		{