gwsw/less

commit cc1bc7c84cadf304a006de2bdc4732515bfb9e73

Fix word boundary search with some regex libraries.
A word boundary search ("\<" in gnu or "\b" in pcre) will sometimes
match strings that do not actually start at a word boundary.
This fixes the problem, when using the gnu, pcre or pcre2 regex
libraries, by using the start offset parameter in the match function.

Related to #611.

pattern.c
@@ -296,7 +296,7 @@ static int match(constant char *pattern, size_t pattern_len, constant char *buf,
  * Set sp[i] and ep[i] to the start and end of the i-th matched subpattern.
  * Subpatterns are defined by parentheses in the regex language.
  */
-static lbool match_pattern1(PATTERN_TYPE pattern, constant char *tpattern, constant char *line, size_t aline_len, constant char **sp, constant char **ep, int nsp, int notbol, int search_type)
+static lbool match_pattern1(PATTERN_TYPE pattern, constant char *tpattern, constant char *line, size_t aline_len, size_t line_off, constant char **sp, constant char **ep, int nsp, int notbol, int search_type)
 {
 	int matched;
 	int line_len = (int) aline_len; /*{{type-issue}}*/
@@ -305,7 +305,7 @@ static lbool match_pattern1(PATTERN_TYPE pattern, constant char *tpattern, const
 	search_type |= SRCH_NO_REGEX;
 #endif
 	if (search_type & SRCH_NO_REGEX)
-		matched = match(tpattern, strlen(tpattern), line, line_len, &sp, &ep, nsp);
+		matched = match(tpattern, strlen(tpattern), line + line_off, line_len - line_off, &sp, &ep, nsp);
 	else
 	{
 #if HAVE_GNU_REGEX
@@ -313,7 +313,7 @@ static lbool match_pattern1(PATTERN_TYPE pattern, constant char *tpattern, const
 		struct re_registers search_regs;
 		pattern->not_bol = notbol;
 		pattern->regs_allocated = REGS_UNALLOCATED;
-		matched = re_search(pattern, line, line_len, 0, line_len, &search_regs) >= 0;
+		matched = re_search(pattern, line, line_len, line_off, line_len - line_off, &search_regs) >= 0;
 		if (matched)
 		{
 			*sp++ = line + search_regs.start[0];
@@ -331,7 +331,7 @@ static lbool match_pattern1(PATTERN_TYPE pattern, constant char *tpattern, const
 		rm[0].rm_so = 0;
 		rm[0].rm_eo = line_len;
 #endif
-		matched = !regexec(pattern, line, RM_COUNT, rm, flags);
+		matched = !regexec(pattern, line + line_off, RM_COUNT, rm, flags);
 		if (matched)
 		{
 			int i;
@@ -368,7 +368,7 @@ static lbool match_pattern1(PATTERN_TYPE pattern, constant char *tpattern, const
 		int i;
 		int ecount;
 		int mcount = pcre_exec(pattern, NULL, line, line_len,
-			0, flags, ovector, OVECTOR_COUNT);
+			line_off, flags, ovector, OVECTOR_COUNT);
 		matched = (mcount > 0);
 		ecount = nsp-1;
 		if (ecount > mcount) ecount = mcount;
@@ -391,7 +391,7 @@ static lbool match_pattern1(PATTERN_TYPE pattern, constant char *tpattern, const
 		int flags = (notbol) ? PCRE2_NOTBOL : 0;
 		pcre2_match_data *md = pcre2_match_data_create(nsp-1, NULL);
 		int mcount = pcre2_match(pattern, (PCRE2_SPTR)line, line_len,
-			0, flags, md, NULL);
+			line_off, flags, md, NULL);
 		matched = (mcount > 0);
 		if (matched)
 		{
@@ -416,21 +416,21 @@ static lbool match_pattern1(PATTERN_TYPE pattern, constant char *tpattern, const
 	}
 #endif
 #if HAVE_RE_COMP
-	matched = (re_exec(line) == 1);
+	matched = (re_exec(line + line_off) == 1);
 	/*
 	 * re_exec doesn't seem to provide a way to get the matched string.
 	 */
 #endif
 #if HAVE_REGCMP
-	matched = ((*ep++ = regex(pattern, line)) != NULL);
+	matched = ((*ep++ = regex(pattern, line + line_off)) != NULL);
 	if (matched)
 		*sp++ = __loc1;
 #endif
 #if HAVE_V8_REGCOMP
 #if HAVE_REGEXEC2
-	matched = regexec2(pattern, line, notbol);
+	matched = regexec2(pattern, line + line_off, notbol);
 #else
-	matched = regexec(pattern, line);
+	matched = regexec(pattern, line + line_off);
 #endif
 	if (matched)
 	{
@@ -459,12 +459,12 @@ static lbool subsearch_ok(constant char **sp, constant char **ep, int search_typ
 	return TRUE;
 }
 
-public lbool match_pattern(PATTERN_TYPE pattern, constant char *tpattern, constant char *line, size_t line_len, constant char **sp, constant char **ep, int nsp, int notbol, int search_type)
+public lbool match_pattern(PATTERN_TYPE pattern, constant char *tpattern, constant char *line, size_t line_len, size_t line_off, constant char **sp, constant char **ep, int nsp, int notbol, int search_type)
 {
 	for (;;)
 	{
 		size_t mlen;
-		lbool matched = match_pattern1(pattern, tpattern, line, line_len, sp, ep, nsp, notbol, search_type);
+		lbool matched = match_pattern1(pattern, tpattern, line, line_len, line_off, sp, ep, nsp, notbol, search_type);
 		if (!matched || subsearch_ok(sp, ep, search_type))
 			return matched;
 		mlen = ep[0] - line;

search.c
@@ -989,8 +989,7 @@ static void create_hilites(POSITION linepos, constant char *line, constant char 
  */
 static void hilite_line(POSITION linepos, constant char *line, size_t line_len, int *chpos, constant char **sp, constant char **ep, int nsp)
 {
-	constant char *searchp;
-	constant char *line_end = line + line_len;
+	size_t line_off = 0;
 
 	/*
 	 * sp[0] and ep[0] delimit the first match in the line.
@@ -1004,7 +1003,6 @@ static void hilite_line(POSITION linepos, constant char *line, size_t line_len, 
 	 * sp[i] and ep[i] for i>0 delimit subpattern matches.
 	 * Color each of them with its unique color.
 	 */
-	searchp = line;
 	do {
 		constant char *lep = sp[0];
 		int i;
@@ -1031,14 +1029,14 @@ static void hilite_line(POSITION linepos, constant char *line, size_t line_len, 
 		 * move to the first char after the string we matched.
 		 * If we matched zero, just move to the next char.
 		 */
-		if (ep[0] > searchp)
-			searchp = ep[0];
-		else if (searchp != line_end)
-			searchp++;
+		if (ep[0] > &line[line_off])
+			line_off = ptr_diff(ep[0], line);
+		else if (line_off != line_len)
+			line_off++;
 		else /* end of line */
 			break;
 	} while (match_pattern(info_compiled(&search_info), search_info.text,
-			searchp, ptr_diff(line_end, searchp), sp, ep, nsp, 1, search_info.search_type));
+			line, line_len, line_off, sp, ep, nsp, 1, search_info.search_type));
 }
 #endif
 
@@ -1179,7 +1177,7 @@ static lbool matches_filters(POSITION pos, char *cline, size_t line_len, int *ch
 	for (filter = filter_infos; filter != NULL; filter = filter->next)
 	{
 		lbool line_filter = match_pattern(info_compiled(filter), filter->text,
-			cline, line_len, sp, ep, nsp, 0, filter->search_type);
+			cline, line_len, 0, sp, ep, nsp, 0, filter->search_type);
 		if (line_filter)
 		{
 			struct hilite hl;
@@ -1655,7 +1653,7 @@ static int search_range(POSITION pos, POSITION endpos, int search_type, int matc
 		if (prev_pattern(&search_info))
 		{
 			line_match = match_pattern(info_compiled(&search_info), search_info.text,
-				cline, line_len, sp, ep, NSP, 0, search_type);
+				cline, line_len, 0, sp, ep, NSP, 0, search_type);
 			if (line_match)
 			{
 				/*

commit 342c928d4e1f0ff21058c4787d72b5056a673489

Fix bug when using POSIX regular expressions.
Broken in cc1bc7c84cadf304a006de2bdc4732515bfb9e73.
If REG_STARTEND is supported, we should start the search at offset
line_off. This not only fixes erroneous behavior, but implements
cc1bc7c84cadf304a006de2bdc4732515bfb9e73 fix for POSIX too.

pattern.c
@@ -326,10 +326,12 @@ static lbool match_pattern1(PATTERN_TYPE pattern, constant char *tpattern, const
 		#define RM_COUNT (NUM_SEARCH_COLORS+2)
 		regmatch_t rm[RM_COUNT];
 		int flags = (notbol) ? REG_NOTBOL : 0;
+
 #ifdef REG_STARTEND
 		flags |= REG_STARTEND;
-		rm[0].rm_so = 0;
+		rm[0].rm_so = line_off;
 		rm[0].rm_eo = line_len;
+		line_off = 0;
 #endif
 		matched = !regexec(pattern, line + line_off, RM_COUNT, rm, flags);
 		if (matched)
@@ -345,12 +347,12 @@ static lbool match_pattern1(PATTERN_TYPE pattern, constant char *tpattern, const
 			{
 				if (rm[i].rm_so < 0)
 				{
-					*sp++ = *ep++ = line;
+					*sp++ = *ep++ = line + line_off;
 				} else
 				{
 #ifndef __WATCOMC__
-					*sp++ = line + rm[i].rm_so;
-					*ep++ = line + rm[i].rm_eo;
+					*sp++ = line + line_off + rm[i].rm_so;
+					*ep++ = line + line_off + rm[i].rm_eo;
 #else
 					*sp++ = rm[i].rm_sp;
 					*ep++ = rm[i].rm_ep;