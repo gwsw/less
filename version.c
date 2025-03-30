369d053462044734178f867abda3942d5273c06d
Mark Nudelman <markn@greenwoodsoftware.com>
2025-03-29 17:57:43 -0700

    Open v675.

--- version.c
+++ version.c
@@ -1043,6 +1043,7 @@ v671  12/6/24   Fix multibyte command chars; fix Windows auto_wrap.
 v672  1/20/25   Don't poll with -F on short file; remove --no-poll.
 v673  3/13/25   Treat -r in LESS var as -R; add ESC-j and ESC-k.
 v674  3/24/25   Improve behavior of ^O^N and ^O^P commands.
+v675
 */
 
-char version[] = "674";
+char version[] = "675x";
