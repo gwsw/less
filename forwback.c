gwsw/less

commit 6f1a946a23d3ded8e5449c8241b203d02b6c29e2

Fix handling of position table in forw().
In order to operate correctly when & filtering is in effect,
we cannot just accept the position in the BOTTOM_PLUS_ONE slot.
This is the next byte after the last line displayed, but may
not be the first byte of the next line to be displayed, due to
filtering.

forwback.c
@@ -221,7 +221,7 @@ public void forw(int n, POSITION pos, lbool force, lbool only_last, lbool to_new
 	int nlines = 0;
 	lbool do_repaint;
 	lbool newline;
-	lbool new_screen = FALSE;
+	lbool first_line = TRUE;
 
 	if (pos != NULL_POSITION)
 		pos = after_header_pos(pos);
@@ -248,7 +248,6 @@ public void forw(int n, POSITION pos, lbool force, lbool only_last, lbool to_new
 			 *    to hit eof in the middle of this screen,
 			 *    but we don't yet know if that will happen. }}
 			 */
-			new_screen = TRUE;
 			pos_clear();
 			force = TRUE;
 			clear();
@@ -262,7 +261,6 @@ public void forw(int n, POSITION pos, lbool force, lbool only_last, lbool to_new
 			 * currently displayed.  Clear the screen image 
 			 * (position table) and start a new screen.
 			 */
-			new_screen = TRUE;
 			pos_clear();
 			force = TRUE;
 			if (top_scroll)
@@ -278,7 +276,7 @@ public void forw(int n, POSITION pos, lbool force, lbool only_last, lbool to_new
 
 	while (--n >= 0)
 	{
-		POSITION linepos;
+		POSITION linepos = NULL_POSITION;
 		/*
 		 * Read the next line of input.
 		 */
@@ -324,12 +322,8 @@ public void forw(int n, POSITION pos, lbool force, lbool only_last, lbool to_new
 		 * Add the position of the next line to the position table.
 		 * Display the current line on the screen.
 		 */
-		if (new_screen)
-		{
-			add_forw_pos(linepos);
-			new_screen = FALSE;
-		}
-		add_forw_pos(pos);
+		add_forw_pos(linepos, first_line);
+		first_line = FALSE;
 		nlines++;
 		if (do_repaint)
 			continue;
@@ -358,6 +352,8 @@ public void forw(int n, POSITION pos, lbool force, lbool only_last, lbool to_new
 			break;
 		forw_prompt = 1;
 	}
+	if (!first_line)
+		add_forw_pos(pos, FALSE);
 	if (nlines == 0 && !ignore_eoi)
 		eof_bell();
 	else if (do_repaint)

position.c
@@ -60,15 +60,18 @@ public POSITION position(int sindex)
 /*
  * Add a new file position to the bottom of the position table.
  */
-public void add_forw_pos(POSITION pos)
+public void add_forw_pos(POSITION pos, lbool no_scroll)
 {
 	int i;
 
 	/*
 	 * Scroll the position table up.
 	 */
-	for (i = 1;  i < sc_height;  i++)
-		table[i-1] = table[i];
+	if (!no_scroll)
+	{
+		for (i = 1;  i < sc_height;  i++)
+			table[i-1] = table[i];
+	}
 	table[sc_height - 1] = pos;
 }
 

commit 63715f66fd0e1d8d1cb0a3dea97eccb32a05b23c

Fix bugs introduced in 6f1a946a23d3ded8e5449c8241b203d02b6c29e2.
If we exit the loop in forw() due to reaching EOF, the final
add_forw_pos should store the position after the last line,
not the useless NULL_POSITION returned from the final forw_line.

Also, if forw_line reads a line longer than the screen width,
it should return in *p_linepos the position of the start of the
screen line, not the raw line.

forwback.c
@@ -308,14 +308,14 @@ public void forw(int n, POSITION pos, lbool force, lbool only_last, lbool to_new
 				 * line in the file reaches the top of screen.
 				 */
 				soft_eof = opos;
-				if (ABORT_SIGS())
+				linepos = opos;
+				if (ABORT_SIGS() ||
+				   (!force && position(TOP) != NULL_POSITION) ||
+				   (!empty_lines(0, 0) && !empty_lines(1, 1) && empty_lines(2, sc_height-1)))
+				{
+					pos = opos;
 					break;
-				if (!force && position(TOP) != NULL_POSITION)
-					break;
-				if (!empty_lines(0, 0) && 
-				    !empty_lines(1, 1) &&
-				     empty_lines(2, sc_height-1))
-					break;
+				}
 			}
 		}
 		/*

input.c
@@ -326,7 +326,7 @@ get_forw_line:
 		new_pos = ch_tell();
 	}
 	if (p_linepos != NULL)
-		*p_linepos = base_pos;
+		*p_linepos = curr_pos;
 	if (p_newline != NULL)
 		*p_newline = endline;
 	return (new_pos);