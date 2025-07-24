gwsw/less

commit 2613f470ca387e5899df667012afed2abc048158

Allow ^X to interrupt read during a search with --incsearch.
In 774cb5e3c590d270f48ca9c7f7933da8ed8e5c70 I said
    This has a risk of getting into a state where iread() is blocked
    waiting for data but is not interruptible by ^X or other non-^C
    chars, but I have not been able to produce such a case.
This case is indeed reproducible, by doing something like
  ( seq 100; while :; do echo a; sleep 1; done ) | ./less --incsearch
and searching for a string that does not exist.

In this change, polling during the search after each char of the pattern
is not fully disabled; instead polling still happens and ^X will still
interrupt a read, but other normal input chars will not interrupt the
read but just be queued for later processing.

os.c
@@ -164,7 +164,8 @@ static int check_poll(int fd, int tty)
 				/* Break out of "waiting for data". */
 				return (READ_INTR);
 			ungetcc_back((char) ch);
-			return (READ_INTR);
+			if (!no_poll)
+				return (READ_INTR);
 		}
 	}
 	if (ignore_eoi && exit_F_on_close && (poller[0].revents & (POLLHUP|POLLIN)) == POLLHUP)
@@ -270,7 +271,7 @@ start:
 	}
 #endif
 #if USE_POLL
-	if (is_tty && fd != tty && use_poll && !no_poll && !(quit_if_one_screen && one_screen))
+	if (is_tty && fd != tty && use_poll && !(quit_if_one_screen && one_screen))
 	{
 		int ret = check_poll(fd, tty);
 		if (ret != 0)
@@ -283,7 +284,7 @@ start:
 	}
 #else
 #if MSDOS_COMPILER==WIN32C
-	if (!no_poll && !(quit_if_one_screen && one_screen) && win32_kbhit2(TRUE))
+	if (!(quit_if_one_screen && one_screen) && win32_kbhit2(TRUE))
 	{
 		int c;
 
@@ -292,7 +293,8 @@ start:
 		reading = FALSE;
 		if (c != CONTROL('C') && c != intr_char)
 			WIN32ungetch((char) c);
-		return (READ_INTR);
+		if (!no_poll)
+			return (READ_INTR);
 	}
 #endif
 #endif

commit 3fdb5f614cd13809307f1af21e8c34a9066ae9cd

Fix 02860b91f0f170ce040cb4efcc42c29ff2156c5b again.
Got broken in 2613f470ca387e5899df667012afed2abc048158.

os.c
@@ -287,14 +287,18 @@ start:
 	if (!(quit_if_one_screen && one_screen) && win32_kbhit2(TRUE))
 	{
 		int c;
+		lbool intr;
 
 		c = WIN32getch();
-		sigs |= S_SWINTERRUPT;
-		reading = FALSE;
-		if (c != CONTROL('C') && c != intr_char)
+		intr = (c == CONTROL('C') || c == intr_char);
+		if (!intr)
 			WIN32ungetch((char) c);
-		if (!no_poll)
+		if (intr || !no_poll)
+		{
+			sigs |= S_SWINTERRUPT;
+			reading = FALSE;
 			return (READ_INTR);
+		}
 	}
 #endif
 #endif