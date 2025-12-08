gwsw/less

commit 67202cb530958a798bd44c39ca4310710a2ee769

New implementation to defer terminal init until data received.
Hopefully this is simpler and more robust than
a55140f63f409cf0069279a6dbaf2d0ea385a2e5.

Related to #682.

output.c
@@ -31,6 +31,7 @@ extern int is_tty;
 extern int oldbot;
 extern int utf_mode;
 extern char intr_char;
+extern lbool term_init_ever;
 
 #if MSDOS_COMPILER==WIN32C || MSDOS_COMPILER==BORLANDC || MSDOS_COMPILER==DJGPPC
 extern int ctldisp;
@@ -416,7 +417,7 @@ public void flush(void)
 	ob = obuf;
 
 #if MSDOS_COMPILER==MSOFTC
-	if (interactive())
+	if (outfd == 1)
 	{
 		obuf[n] = '\0';
 		_outtext(obuf);
@@ -424,7 +425,7 @@ public void flush(void)
 	}
 #else
 #if MSDOS_COMPILER==WIN32C || MSDOS_COMPILER==BORLANDC || MSDOS_COMPILER==DJGPPC
-	if (interactive())
+	if (outfd == 1)
 	{
 		ob = obuf + n;
 		*ob = '\0';
@@ -454,8 +455,17 @@ public void set_output(int fd)
 public int putchr(int ch)
 {
 	char c = (char) ch;
+
+	/*
+	 * Init the terminal if thiss is the first byte written to stdout
+	 * (rather than stderr), and the terminal has never been initted.
+	 * If it has previously been initted, it will be reinitted explicitly
+	 * as part of a term_deinit/term_init pair, so we shouldn't do it here.
+	 */
+	if (!term_init_ever && outfd == 1)
+		term_init();
+
 #if 0 /* fake UTF-8 output for testing */
-	extern int utf_mode;
 	if (utf_mode)
 	{
 		static char ubuf[MAX_UTF_CHAR_LEN];

main.c
@@ -78,6 +78,7 @@ extern int      errmsgs;
 extern int      redraw_on_quit;
 extern int      term_addrs;
 extern lbool    first_time;
+extern lbool    term_init_ever;
 
 #if MSDOS_COMPILER==WIN32C && (defined(__MINGW32__) || defined(_MSC_VER))
 /* malloc'ed 0-terminated utf8 of 0-terminated wide ws, or null on errors */
@@ -413,6 +414,7 @@ int main(int argc, constant char *argv[])
 		 * Just copy the input file(s) to output.
 		 */
 		set_output(1); /* write to stdout */
+		term_init_ever = TRUE; /* don't init terminal in putchr */
 		SET_BINARY(1);
 		if (edit_first() == 0)
 		{
@@ -497,7 +499,6 @@ int main(int argc, constant char *argv[])
 		putchr('\n');
 	}
 	set_output(1);
-	term_init();
 	commands();
 	quit(QUIT_OK);
 	/*NOTREACHED*/

os.c
@@ -425,7 +425,9 @@ public void intio(void)
  */
 public void have_read_data(void)
 {
+#if USE_POLL
 	any_data = TRUE;
+#endif
 }
 
 /*

screen.c
@@ -259,6 +259,7 @@ static int attrcolor = -1;
 
 /* term_init has been called; terminal is ready for use by less */
 static lbool term_init_done = FALSE;
+public lbool term_init_ever = FALSE;
 
 public int auto_wrap;           /* Terminal does \r\n when write past margin */
 public int defer_wrap;          /* After printing char in last column, doesn't wrap until next char */
@@ -1953,7 +1954,7 @@ public void term_init(void)
 {
 	if (term_init_done)
 		return;
-	term_init_done = TRUE;
+	term_init_done = term_init_ever = TRUE;
 	clear_bot_if_needed();
 #if !MSDOS_COMPILER
 	if (!(quit_if_one_screen && one_screen))
@@ -2061,8 +2062,6 @@ public int interactive(void)
 
 static void assert_interactive(void)
 {
-	if (!interactive())
-		error("Internal error: terminal not initialized", NULL_PARG);
 }
 
 /*

commit 6544229ada5beab276079e18eef9ff63138dcba3

Fix non-tty output on Windows version.
The test in flush() for whether to process ANSI sequences or
just write straight to outfd on Windows was broken in
67202cb530958a798bd44c39ca4310710a2ee769. The previous logic
which called interactive() was correct.

output.c
@@ -417,7 +417,7 @@ public void flush(void)
 	ob = obuf;
 
 #if MSDOS_COMPILER==MSOFTC
-	if (outfd == 1)
+	if (interactive())
 	{
 		obuf[n] = '\0';
 		_outtext(obuf);
@@ -425,7 +425,7 @@ public void flush(void)
 	}
 #else
 #if MSDOS_COMPILER==WIN32C || MSDOS_COMPILER==BORLANDC || MSDOS_COMPILER==DJGPPC
-	if (outfd == 1)
+	if (interactive())
 	{
 		ob = obuf + n;
 		*ob = '\0';