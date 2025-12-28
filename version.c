5b6ca1e413a976ba98613fb2d8e61c4d409fb1f4
Mark Nudelman <markn@greenwoodsoftware.com>
2025-12-27 21:26:00 -0800

    v689

--- version.c
+++ version.c
@@ -1069,9 +1069,9 @@ v687  11/17/25  Disallow opening OSC8 link via mouse in secure mode;
                 add --autosave; make SIGHUP act like SIGTERM; add ESC-f.
 v688  11/28/25  Add LESSNOCONFIG; defer "ti" until first char received;
                 don't auto-bold line numbers.
-v689            Add prompt seqs %C, %W, %Q, ?Q; don't allow any command to
+v689  12/27/25  Add prompt seqs %C, %W, %Q, ?Q; don't allow any command to
                 interrupt waiting for data; fix screen resize on Windows;
                 add --without-termlib.
 */
 
-char version[] = "689x";
+char version[] = "689";
