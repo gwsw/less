gwsw/less

commit 04ab72cea3da04099ea70955f50152045009e93a

Fix bug in command decoder when a command is a prefix of another.
If command A is a prefix of command B, and command A appears in
a lesskey file before command B, then a search for command A
would correctly find the table entry for A, but then when it
encountered B in the table it would decide that the command is
a prefix of B and forget that it had an exact match for A.

Related to #626.

decode.c
@@ -750,7 +750,7 @@ static int cmd_search(constant char *cmd, constant unsigned char *table, constan
 			{
 				action = taction;
 				*extra = textra;
-			} else if (match > 0) /* cmd is a prefix of this table entry */
+			} else if (match > 0 && action == A_INVALID) /* cmd is a prefix of this table entry */
 			{
 				action = A_PREFIX;
 			}

commit 8697a6f48eda7709b25ba80d7b877e159637276b

Fix regression in command parser.
0511532b5d367996f80bba655e64ba03e0940ae7 rewrote the command parser
to allow matching only the tail of a command string, so that entering
an invalid command followed by a valid one would match the valid part.
d8aa003e20e1a8a2d2252ee01a91c7bd7e8ebe20 fixed a bug in the new
parser which would accept an exact match instead of a longer partial
match (so, for example, "\e[1..." would immediately match the "1" as
a digit command).
This was broken again in 04ab72cea3da04099ea70955f50152045009e93a
which fixed a problem when one command or env var is a prefix of
another, but reintroduced the bug fixed by
d8aa003e20e1a8a2d2252ee01a91c7bd7e8ebe20.
It also broke the --no-paste option.

This commit fixes that regression, so a longer partial match takes
precedence over a shorter exact match.
This also makes --no-paste work correctly again.

Related to #724.

decode.c
@@ -800,9 +800,11 @@ static int cmd_search(constant char *cmd, constant unsigned char *table, constan
 				action = taction;
 				if (extra != NULL)
 					*extra = textra;
-			} else if (match > 0 && action == A_INVALID) /* cmd is a prefix of this table entry */
+			} else if (match > 0 && (match > match_len || action == A_INVALID)) /* cmd is a prefix of this table entry */
 			{
 				action = A_PREFIX;
+				if (extra != NULL)
+					*extra = NULL;
 			}
 			match_len = match;
 		}