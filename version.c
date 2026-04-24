53be2c2e914689c603e0d1338170c162aedc35bd
Mark Nudelman <markn@greenwoodsoftware.com>
2026-04-23 19:59:38 -0700

    Open v700.

--- version.c
+++ version.c
@@ -1102,6 +1102,7 @@ v697  4/19/26   Fix bug using PCRE2 with more than 5 parentheses; discard
 v698  4/23/26   Fix bad display after message in narrow terminal; replace
                 LESS_OSC8_xxx with LESS_OSC8_OPEN_xxx; add \kpe to lesskey.
 v699  4/23/26   Add less-osc8-*.bash files to distribution.
+v700
 */
 
-char version[] = "699";
+char version[] = "700x";
