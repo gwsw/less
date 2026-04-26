37118f1a9aea56560c1b0d6593b5df3d8d6e0105
Mark Nudelman <markn@greenwoodsoftware.com>
2026-04-26 09:35:21 -0700

    Allow OSC 8 man link with non-numeric section name.
    Allow an OSC 8 hyperlink to a man page to have a section name
    which is not all digits, like "man:printf(3p)".

--- less-osc8-man.bash
+++ less-osc8-man.bash
@@ -2,7 +2,7 @@
 # Less OSC 8 handler for man links.
 # Open a link of the form "man:NAME" or "man:NAME(SECTION)".
 
-if [[ "$1" =~ ^man:([^\(]+)(\(([0-9]+)\))?$ ]]; then
+if [[ "$1" =~ ^man:([^\(]+)(\((.+)\))?$ ]]; then
     _NAME="${BASH_REMATCH[1]}"
     _SECT="${BASH_REMATCH[3]}"
     if [ -z "$_SECT" ]; then
