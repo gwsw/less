fc7dafdbdfd1c26ed839d77f13428419cccd44fe
Mark Nudelman <markn@greenwoodsoftware.com>
2026-05-22 09:29:27 -0700

    v702

--- version.c
+++ version.c
@@ -1106,7 +1106,7 @@ v700  4/30/26   Add \kpe to lesskey; improve less-osc8-* scripts.
 v701  5/13/26   Replace less-osc-*.bash with less-osc8-open.sh; don't init
                 terminal if stdout is not tty; fix bug clicking OSC 8 link
                 in a long line; fix misread chars on Windows; remove %O.
-v702
+v702  5/22/26   Doc.
 */
 
-char version[] = "702x";
+char version[] = "702";
