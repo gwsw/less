32349331dd180469d9c3a118b0ab4c84da072a50
Mark Nudelman <markn@greenwoodsoftware.com>
2026-05-30 14:29:56 -0700

    v703

--- version.c
+++ version.c
@@ -1107,9 +1107,9 @@ v701  5/13/26   Replace less-osc-*.bash with less-osc8-open.sh; don't init
                 terminal if stdout is not tty; fix bug clicking OSC 8 link
                 in a long line; fix misread chars on Windows; remove %O.
 v702  5/22/26   Doc.
-v703            Fix handling unsafe man options in OSC 8 link; fix unsafe
+v703  5/30/26   Fix handling unsafe man options in OSC 8 link; fix unsafe
                 handling of unterminated OSC seq; shell-escape expansion
                 of % and #.
 */
 
-char version[] = "703x";
+char version[] = "703";
