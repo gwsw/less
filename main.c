gwsw/less

commit 6f498e33fafea29e756d29d4b45a807b1900d3d8

Add LESSNOCONFIG environment variable.
Provides an easy way to disable lesskey files and either all
environment variables or all variables except those from a
specified list. TERM is never ignored.

Related to #688.

main.c
@@ -40,6 +40,7 @@ public constant char *progname;
 public lbool    quitting = FALSE;
 public int      dohelp;
 public char *   init_header = NULL;
+public char *   no_config = NULL;
 static int      secure_allow_features;
 
 #if LOGFILE
@@ -205,6 +206,12 @@ static int security_feature(constant char *name, size_t len)
 		return security_feature_error("invalid", len, name);
 	return features[match].sf_value;
 }
+
+static lbool set_security_feature(constant char *word, size_t wlen, void *arg)
+{
+	secure_allow_features |= security_feature(word, wlen);
+	return TRUE;
+}
 #endif /* !SECURE */
 
 /*
@@ -224,19 +231,7 @@ static void init_secure(void)
 
 	str = lgetenv("LESSSECURE_ALLOW");
 	if (!isnullenv(str))
-	{
-		for (;;)
-		{
-			constant char *estr;
-			while (*str == ' ' || *str == ',') ++str; /* skip leading spaces/commas */
-			if (*str == '\0') break;
-			estr = strchr(str, ',');
-			if (estr == NULL) estr = str + strlen(str);
-			while (estr > str && estr[-1] == ' ') --estr; /* trim trailing spaces */
-			secure_allow_features |= security_feature(str, ptr_diff(estr, str));
-			str = estr;
-		}
-	}
+		parse_csl(set_security_feature, str, NULL);
 #endif
 }
 
@@ -254,6 +249,8 @@ int main(int argc, constant char *argv[])
 	size_t f;
 	lbool end_opts = FALSE;
 	lbool posixly_correct = FALSE;
+
+	no_config = getenv("LESSNOCONFIG");
 
 #if MSDOS_COMPILER==WIN32C && (defined(__MINGW32__) || defined(_MSC_VER))
 	if (GetACP() != CP_UTF8)  /* not using a UTF-8 manifest */
@@ -323,7 +320,7 @@ int main(int argc, constant char *argv[])
 
 #define isoptstring(s)  (((s)[0] == '-' || (s)[0] == '+') && (s)[1] != '\0')
 	xbuf_init(&xfiles);
-	posixly_correct = (getenv("POSIXLY_CORRECT") != NULL);
+	posixly_correct = (lgetenv("POSIXLY_CORRECT") != NULL);
 	for (i = 0;  i < argc;  i++)
 	{
 		if (strcmp(argv[i], "--") == 0)

decode.c
@@ -36,6 +36,7 @@
 extern int erase_char, erase2_char, kill_char;
 extern int mousecap;
 extern int sc_height;
+extern char *no_config;
 
 static constant lbool allow_drag = TRUE;
 
@@ -867,6 +868,46 @@ public int ecmd_decode(constant char *cmd, constant char **sp)
 	return (cmd_decode(list_ecmd_tables, cmd, sp));
 }
 
+/*
+ * Parse a comma-separated list.
+ * Call func repeatedly, passing each item in the list.
+ * Stop and return FALSE if func ever returns FALSE,
+ * otherwise parse the entire list and return TRUE.
+ */
+public lbool parse_csl(lbool (*func)(constant char *word, size_t wlen, void *arg), constant char *str, void *arg)
+{
+	for (;;)
+	{
+		constant char *estr;
+		while (*str == ' ' || *str == ',') ++str; /* skip leading spaces/commas */
+		if (*str == '\0') break;
+		estr = strchr(str, ',');
+		if (estr == NULL) estr = str + strlen(str);
+		while (estr > str && estr[-1] == ' ') --estr; /* trim trailing spaces */
+		if (!(*func)(str, ptr_diff(estr, str), arg))
+			return FALSE;
+		str = estr;
+	}
+	return TRUE;
+}
+
+/*
+ * Should we ignore the setting of an environment variable?
+ */
+static lbool not_in_no_config(constant char *word, size_t wlen, void *arg)
+{
+	constant char *var = (constant char *) arg;
+	if (wlen == strlen(var) && strncmp(var, word, wlen) == 0)
+		return FALSE;
+	return TRUE;
+}
+static lbool ignore_env(constant char *var)
+{
+	if (no_config == NULL)
+		return FALSE; /* no_config is not set; don't ignore anything */
+	/* no_config is set; ignore any var that does not appear in no_config */
+	return parse_csl(not_in_no_config, no_config, (void*) var);
+}
 
 /*
  * Get the value of an environment variable.
@@ -877,6 +918,8 @@ public constant char * lgetenv(constant char *var)
 	int a;
 	constant char *s;
 
+	if (ignore_env(var))
+		return (NULL);
 	a = cmd_decode(list_var_tables, var, &s);
 	if (a == EV_OK)
 		return (s);
@@ -1032,7 +1075,7 @@ public int lesskey(constant char *filename, lbool sysvar)
 	ssize_t n;
 	int f;
 
-	if (!secure_allow(SF_LESSKEY))
+	if (!secure_allow(SF_LESSKEY) || no_config != NULL)
 		return (1);
 	/*
 	 * Try to open the lesskey file.
@@ -1094,7 +1137,7 @@ static int lesskey_text(constant char *filename, lbool sysvar, lbool content)
 	int r;
 	static struct lesskey_tables tables;
 
-	if (!secure_allow(SF_LESSKEY))
+	if (!secure_allow(SF_LESSKEY) || no_config != NULL)
 		return (1);
 	r = content ? parse_lesskey_content(filename, &tables) : parse_lesskey(filename, &tables);
 	if (r != 0)

less.nro.VER
@@ -2691,6 +2691,14 @@ so scrolling may be slower.
 Duration (in milliseconds) after starting to read data from the input,
 after which the "Waiting for data" message will be displayed.
 The default is 4000 (4 seconds).
+.IP LESSNOCONFIG
+If set to an empty string or "-", all environment variables except TERM are ignored
+(treated as if they were not set).
+If set to a comma-separated list of environment variable names,
+all environment variables except TERM and those in the list are ignored.
+If set to any value, all
+.B lesskey
+files are also ignored.
 .IP LESS_IS_MORE
 Emulate the
 .BR more (1)

screen.c
@@ -1409,7 +1409,7 @@ public void get_term(void)
 	/*
 	 * Find out what kind of terminal this is.
 	 */
-	if ((term = lgetenv("TERM")) == NULL)
+	if ((term = lgetenv("TERM")) == NULL && (term = getenv("TERM")) == NULL)
 		term = DEFAULT_TERM;
 	hardcopy = 0;
 #if USE_TERMINFO

version.c
@@ -1067,7 +1067,7 @@ v686  10/30/25  Map keypad keys; make HOME/END scroll horizontally;
                 values to cmd line options.
 v687  11/17/25  Disallow opening OSC8 link via mouse in secure mode;
                 add --autosave; make SIGHUP act like SIGTERM; add ESC-f.
-v688
+v688            Add LESSNOCONFIG.
 */
 
 char version[] = "688x";

commit 81ba76ffb6b96f9ceb7914d7fa1a42aba2b23450

Fix bug in handling POSIXLY_CORRECT.
We must get the value of POSIXLY_CORRECT using getenv not lgetenv,
because POSIXLY_CORRECT should be considered true if it is set to
any value, even an empty string, but lgetenv returns NULL if it is
set to an empty string.

This was broken in 6f498e33fafea29e756d29d4b45a807b1900d3d8.

main.c
@@ -284,7 +284,9 @@ int main(int argc, constant char *argv[])
 
 #define isoptstring(s)  (((s)[0] == '-' || (s)[0] == '+') && (s)[1] != '\0')
 	xbuf_init(&xfiles);
-	posixly_correct = (lgetenv("POSIXLY_CORRECT") != NULL);
+	/* Don't use lgetenv because POSIXLY_CORRECT is considered to be true
+	 * if it is set to an empty string. */
+	posixly_correct = (getenv("POSIXLY_CORRECT") != NULL);
 	for (i = 0;  i < argc;  i++)
 	{
 		if (strcmp(argv[i], "--") == 0)