36e9ac8e2ac194752d03ecc6dd09197032a6cfdf
Mark Nudelman <markn@greenwoodsoftware.com>
2026-06-23 17:57:40 -0700

    Doc.

--- version.c
+++ version.c
@@ -1112,8 +1112,8 @@ v703  5/30/26   Fix handling unsafe man options in OSC 8 link; fix unsafe
                 of % and #.
 v704  5/30/26   Fix uninstall target in Makefile.
 v705  6/15/26   Fix bug overriding built-in command.
-v706  6/22/26   Add ^ and + modifiers in lesskey file; support ctl and shift
-                up/down arrows; add -DO; add LESSSECURE_DISALLOW;
+v706  6/22/26   Add ^ and + modifiers for \k in lesskey file; support ctl 
+                and shift up/down arrows; add -DO; add LESSSECURE_DISALLOW;
                 add LESS_SHELL_COPTION; add %G,%y,%% in prompts; underline
                 OSC 8 links; use separate names for lesstest executable;
                 add getenv cache; improve error handling for bad lesskey files
