a996e1bcbe4fbf737e2defdab7c0c977cd11aab6
Mark Nudelman <markn@greenwoodsoftware.com>
2026-09-05 13:47:19 -0700

    Doc.

--- version.c
+++ version.c
@@ -1127,7 +1127,8 @@ v709  8/15/26   Fix --rscroll with non-ASCII char; fix exit with invalid TERM;
                 starting with '!'; add ESC-]; support shift/ctl page up/down;
                 fix search ^S bugs; fix display issues with less_printf.
 v710            Fix string overrrun if input contains "\e\0"; fix memory leak
-                when using ${...} in lesskey file.
+                when using ${...} in lesskey file; fix status column when long
+                line is marked.
 */
 
 char version[] = "710x";
